bb9a1f2ed1ec5b2b4d8f0791f1938dad2c14d88a
Eran Agmon <agmon.eran@gmail.com>
2021-01-19 10:53:41 -0800

    start migration

---
+++ utils/migration.py
@@ -0,0 +1,19 @@
+
+from ecoli.composites.ecoli_master import Ecoli
+
+def report_process_update():
+    pass
+
+
+
+
+def run_migration_check():
+    ecoli = Ecoli({'agent_id': '1'})
+
+    import ipdb; ipdb.set_trace()
+
+
+
+
+if __name__ == '__main__':
+    run_migration_check()
