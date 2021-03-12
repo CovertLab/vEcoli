da7a980493dd7fc4d01db7f15d3ff063e1e4fc9e
Michael <michaelyang42@gmail.com>
2021-03-11 22:26:02 -0800

    unpacking updates from vivarium and wcEcoli

--- ecoli/migration/transcript_initiation.py
+++ ecoli/migration/transcript_initiation.py
@@ -46,12 +46,50 @@ def test_transcript_initiation():
     actual_update = run_ecoli_process(process, topology)
 
     # separate the update to its ports
+    rna_snyth_prob = actual_update['listeners']['rna_synth_prob']['rna_synth_prob']
 
-    # compare to collected update from ecEcoli
+    rib_data = actual_update['listeners']['ribosome_data']
+    n_5SrRNA_prod = rib_data['rrn5S_produced']
+    n_16SrRNA_prod = rib_data['rrn16S_produced']
+    n_23SrRNA_prod = rib_data['rrn23S_produced']
+    init_prob_5SrRNA = rib_data['rrn5S_init_prob']
+    init_prob_16SrRNA = rib_data['rrn16S_init_prob']
+    init_prob_23SrRNA = rib_data['rrn23S_init_prob']
+    total_rna_init = rib_data['total_rna_init']
+
+    rnap_data = actual_update['listeners']['rnap_data']
+    assert total_rna_init == rnap_data['didInitialize']
+    rna_inits = rnap_data['rnaInitEvent']
+
+    active_RNAPs = actual_update['active_RNAPs']['_add']
+    d_inactive_RNAPs = actual_update['molecules']['inactive_RNAPs']
+    assert d_inactive_RNAPs == -total_rna_init
+    RNAs = actual_update['RNAs']['_add']
+
+    # compare to collected update from wcEcoli
     with open("data/transcript_initiation_update_t2.json") as f:
         wc_data = json.load(f)
 
     # unpack wc_data
+    wc_rna_synth_prob = wc_data['listeners']['rna_synth_prob']['rna_synth_prob']
+
+    wc_rib_data = wc_data['listeners']['ribosome_data']
+    wc_n_5SrRNA_prod = wc_rib_data['rrn5S_produced']
+    wc_n_16SrRNA_prod = wc_rib_data['rrn16S_produced']
+    wc_n_23SrRNA_prod = wc_rib_data['rrn23S_produced']
+    wc_init_prob_5SrRNA = wc_rib_data['rrn5S_init_prob']
+    wc_init_prob_16SrRNA = wc_rib_data['rrn16S_init_prob']
+    wc_init_prob_23SrRNA = wc_rib_data['rrn23S_init_prob']
+    wc_total_rna_init = wc_rib_data['total_rna_init']
+
+    wc_rnap_data = wc_data['listeners']['rnap_data']
+    assert wc_total_rna_init == wc_rnap_data['didInitialize']
+    wc_rna_inits = wc_rnap_data['rnaInitEvent']
+
+    wc_active_RNAPs = wc_data['active_RNAPs']['_add']
+    wc_d_inactive_RNAPs = wc_data['molecules']['APORNAP-CPLX[c]']
+    assert wc_d_inactive_RNAPs == -wc_total_rna_init
+    wc_RNAs = wc_data['RNAs']['_add']
 
     # Sanity checks: ...
 
