9d27389ab51d01bbcfe31ecb04587f2d0d5d1606
Eran Agmon <agmon.eran@gmail.com>
2021-05-03 08:28:00 -0700

    clean up generate_processes

--- ecoli/composites/ecoli_master.py
+++ ecoli/composites/ecoli_master.py
@@ -19,7 +19,6 @@ from ecoli.library.sim_data import LoadSimData
 
 # vivarium processes
 from vivarium.processes.divide_condition import DivideCondition
-from vivarium.processes.meta_division import MetaDivision
 
 # vivarium-ecoli processes
 from ecoli.processes.tf_binding import TfBinding
@@ -49,6 +48,7 @@ class Ecoli(Composer):
         'seed': 0,
         'sim_data_path': SIM_DATA_PATH,
         'daughter_path': tuple(),
+        'division': {'threshold': 2220},  # fg
     }
 
     def __init__(self, config):
@@ -65,62 +65,37 @@ class Ecoli(Composer):
         time_step = config['time_step']
         parallel = config['parallel']  # TODO (Eran) -- which processes can be parallelized?
 
-        # initialize processes
-        tf_binding = TfBinding(
-            self.load_sim_data.get_tf_config(time_step=time_step))
-        transcript_initiation = TranscriptInitiation(
-            self.load_sim_data.get_transcript_initiation_config(time_step=time_step))
-        transcript_elongation = TranscriptElongation(
-            self.load_sim_data.get_transcript_elongation_config(time_step=time_step))
-        rna_degradation = RnaDegradation(
-            self.load_sim_data.get_rna_degradation_config(time_step=time_step))
-        polypeptide_initiation = PolypeptideInitiation(
-            self.load_sim_data.get_polypeptide_initiation_config(time_step=time_step))
-        polypeptide_elongation = PolypeptideElongation(
-            self.load_sim_data.get_polypeptide_elongation_config(time_step=time_step))
-        complexation = Complexation(
-            self.load_sim_data.get_complexation_config(time_step=time_step))
-        two_component_system = TwoComponentSystem(
-            self.load_sim_data.get_two_component_system_config(time_step=time_step))
-        equilibrium = Equilibrium(
-            self.load_sim_data.get_equilibrium_config(time_step=time_step))
-        protein_degradation = ProteinDegradation(
-            self.load_sim_data.get_protein_degradation_config(time_step=time_step))
-        metabolism = Metabolism(
-            self.load_sim_data.get_metabolism_config(time_step=time_step))
-        mass = Mass(
-            self.load_sim_data.get_mass_config(time_step=time_step))
+        # get the configs from sim_data
+        tf_binding_config = self.load_sim_data.get_tf_config(time_step=time_step)
+        transcript_initiation_config = self.load_sim_data.get_transcript_initiation_config(time_step=time_step)
+        transcript_elongation_config = self.load_sim_data.get_transcript_elongation_config(time_step=time_step)
+        rna_degradation_config = self.load_sim_data.get_rna_degradation_config(time_step=time_step)
+        polypeptide_initiation_config = self.load_sim_data.get_polypeptide_initiation_config(time_step=time_step)
+        polypeptide_elongation_config = self.load_sim_data.get_polypeptide_elongation_config(time_step=time_step)
+        complexation_config = self.load_sim_data.get_complexation_config(time_step=time_step)
+        two_component_system_config = self.load_sim_data.get_two_component_system_config(time_step=time_step)
+        equilibrium_config = self.load_sim_data.get_equilibrium_config(time_step=time_step)
+        protein_degradation_config = self.load_sim_data.get_protein_degradation_config(time_step=time_step)
+        metabolism_config = self.load_sim_data.get_metabolism_config(time_step=time_step)
+        mass_config = self.load_sim_data.get_mass_config(time_step=time_step)
 
-        # Division
-        # TODO -- get mass for division from sim_data
-        # TODO -- set divider to binomial division
-        divide_config = {'threshold': 2220}  # fg
-        divide_condition = DivideCondition(divide_config)
-
-        # daughter_path = config['daughter_path']
-        # agent_id = config.get('agent_id', str(uuid.uuid1()))
-        # division_config = dict(
-        #     config.get('division', {}),
-        #     daughter_path=daughter_path,
-        #     agent_id=agent_id,
-        #     compartment=self)
-        # meta_division = MetaDivision(division_config)
+        # additional processes
+        divide_config = config['division']
 
         return {
-            'tf_binding': tf_binding,
-            'transcript_initiation': transcript_initiation,
-            'transcript_elongation': transcript_elongation,
-            'rna_degradation': rna_degradation,
-            'polypeptide_initiation': polypeptide_initiation,
-            # 'polypeptide_elongation': polypeptide_elongation,
-            'complexation': complexation,
-            'two_component_system': two_component_system,
-            'equilibrium': equilibrium,
-            'protein_degradation': protein_degradation,
-            'metabolism': metabolism,
-            'mass': mass,
-            'divide_condition': divide_condition,
-            # 'division': meta_division,
+            'tf_binding': TfBinding(tf_binding_config),
+            'transcript_initiation': TranscriptInitiation(transcript_initiation_config),
+            'transcript_elongation': TranscriptElongation(transcript_elongation_config),
+            'rna_degradation': RnaDegradation(rna_degradation_config),
+            'polypeptide_initiation': PolypeptideInitiation(polypeptide_initiation_config),
+            # 'polypeptide_elongation': PolypeptideElongation(polypeptide_elongation_config),
+            'complexation': Complexation(complexation_config),
+            'two_component_system': TwoComponentSystem(two_component_system_config),
+            'equilibrium': Equilibrium(equilibrium_config),
+            'protein_degradation': ProteinDegradation(protein_degradation_config),
+            'metabolism': Metabolism(metabolism_config),
+            'mass': Mass(mass_config),
+            'divide_condition': DivideCondition(divide_config),
         }
 
     def generate_topology(self, config):
@@ -220,11 +195,8 @@ class Ecoli(Composer):
                 'divide': ('globals', 'divide',),
             },
 
-            # 'division': {
-            #     'global': boundary_path,
-            #     'agents': agents_path
-            # },
         }
+
 
 
 def infinitize(value):
