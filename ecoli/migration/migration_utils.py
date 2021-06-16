767dc445611be7b4873588df2815285194211e8e
Michael <michaelyang42@gmail.com>
2021-06-15 21:47:37 -0500

    update documentation, run_all

--- ecoli/migration/migration_utils.py
+++ ecoli/migration/migration_utils.py
@@ -117,15 +117,47 @@ def percent_error(actual, expected):
     return abs((actual - expected) / expected)
 
 
-class MigrationTestSuite:
+class ComparisonTestSuite:
+    '''
+    Class for running comparison tests in a consistent and reproducible way.
+    The key function here is
+
+        ComparisonTestSuite.run_tests(vivEcoli_update, wcEcoli_update, ...)
+
+    which expects the two updates to compare in dictionary form, with the same structure.
+
+    To create a ComparisonTestSuite, first define your expectations in a test structure.
+    This should be a dictionary of the same structure as the two updates to be compared,
+    but with the leaf nodes replaced with a (two-input) "test function" that compares
+    the corresponding elements of the two updates by
+
+    1. returning False if they are different (something Truth-y otherwise), and
+    2. (optionally) returning additional diagnostic information.
+
+    For example:
+
+        def scalar_equal(v1, v2):
+            return v1==v2, f"Difference (actual-expected) is {v1-v2}"
+
+    Creating a ComparisonTestSuite with fail_loudly = True causes it to stop testing
+    as soon as it encounters the first error. Otherwise, failures occur silently.
+
+    Results of testing can be viewed using ComparisonTestSuite.dump_report(), or
+    by examining ComparisonTestSuite.report manually.
+
+    To raise an AssertionError after running
+    '''
+
     def __init__(self, test_structure, fail_loudly=False):
         self.test_structure = test_structure
         self.fail_loudly = fail_loudly
-
-    def run_tests(self, vivEcoli_update, wcEcoli_update, verbose=False):
         self.report = {}
 
-        def run_tests_iter(vivEcoli_update, wcEcoli_update, test_structure, verbose, level=0):
+    def run_tests(self, vivEcoli_update, wcEcoli_update, verbose=False):
+        failures
+
+        def run_tests_iter(vivEcoli_update, wcEcoli_update, test_structure,
+                           verbose, level=0):
             for k, v in test_structure.items():
                 if verbose:
                     print('.'*level + f'Testing {k}...')
@@ -140,11 +172,12 @@ class MigrationTestSuite:
                 if callable(v):
                     try:
                         print('.'*level + 'Running test\n' +
-                              '.'*level + f'\t{v}\n'+
+                              '.'*level + f'\t{v}\n' +
                               '.'*level + 'for element {k}.')
                         test_result = v(vivEcoli_update[k], wcEcoli_update[k])
                         #self.report[k] = test_result if test_result is not None else True
                     except AssertionError as e:
+                        self.failure = e
                         if self.fail_loudly:
                             raise e
                         if verbose:
@@ -162,10 +195,20 @@ class MigrationTestSuite:
 
 
 def array_equal(arr1, arr2):
-    np.testing.assert_array_equal(arr1, arr2)
+    return np.array_equal(arr1, arr2, equal_nan=True)
 
 def scalar_equal(v1, v2):
-    assert v1==v2
+    return v1==v2, f"Difference (actual-expected) is {v1-v2}"
 
-def array_almost_equal(tol=0.05):
-    return lambda arr1, arr2: max(percent_error(a, b) for a, b in zip(arr1, arr2)) < tol
+def run_all(*tests):
+    def _run_all(arr1, arr2):
+        result = []
+        for test in tests:
+            result.append(test(arr1, arr2))
+
+        result = (all([x[0] if isinstance(x, tuple) else x for x in result]),
+                  [x[1] if isinstance(x, tuple) else "" for x in result])
+
+        return result
+
+    return _run_all
