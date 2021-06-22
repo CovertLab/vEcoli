29cfe995c020dca1b1fbc85f1a340342a1fe44a7
Eran Agmon <agmon.eran@gmail.com>
2021-06-21 18:28:58 -0700

    test_library for chromosome migration file

--- ecoli/migration/chromosome_replication.py
+++ ecoli/migration/chromosome_replication.py
@@ -1,4 +1,5 @@
 
+import argparse
 import numpy as np
 
 from vivarium.core.experiment import pf
@@ -32,7 +33,7 @@ topology = {
 }
 
 
-def test_chromosome_replication_default():
+def test_actual_update():
     config = load_sim_data.get_chromosome_replication_config()
     chromosome_replication = ChromosomeReplication(config)
 
@@ -43,8 +44,26 @@ def test_chromosome_replication_default():
         total_time=2,
         initial_time=1000)
 
-    print(actual_update)
+    print(pf(actual_update))
 
+
+def test_chromosome_replication_default():
+    config = load_sim_data.get_chromosome_replication_config()
+    chromosome_replication = ChromosomeReplication(config)
+
+    # get the initial state
+    initial_state = get_state_from_file(
+        path=f'data/wcecoli_t1000.json')
+
+    # get relevant initial state and experiment
+    state_before, experiment = get_process_state(chromosome_replication, topology, initial_state)
+
+    # run experiment
+    experiment.update(4)
+    data = experiment.emitter.get_data()
+
+    print(pf(data))
+    # import ipdb; ipdb.set_trace()
 
 
 def test_initiate_replication():
@@ -66,28 +85,24 @@ def test_initiate_replication():
     experiment.update(4)
     data = experiment.emitter.get_data()
 
-    # print(f'BEFORE: {pf(state_before)}')
     print(pf(data))
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
 
+    # TODO -- is delete coming through?
+    data[1.0]['oriC']
 
 
 def test_fork_termination():
     config = load_sim_data.get_chromosome_replication_config()
 
-    # change replichore_length parameter to force termination
+    # change replichore_length parameter to force early termination
     config['replichore_lengths'] = np.array([1897318, 1897318])
-
 
     chromosome_replication = ChromosomeReplication(config)
 
     # get the initial state
     initial_state = get_state_from_file(
         path=f'data/wcecoli_t1000.json')
-
-    # # increase cell_mass to trigger replication initiation
-    # cell_mass = 2000.0
-    # initial_state['listeners']['mass']['cell_mass'] = cell_mass
 
     # get relevant initial state and experiment
     state_before, experiment = get_process_state(chromosome_replication, topology, initial_state)
@@ -96,12 +111,26 @@ def test_fork_termination():
     experiment.update(4)
     data = experiment.emitter.get_data()
 
-    # print(f'BEFORE: {pf(state_before)}')
     print(pf(data))
-    import ipdb; ipdb.set_trace()
+    # import ipdb; ipdb.set_trace()
 
 
-if __name__ == "__main__":
-    # test_chromosome_replication_default()
-    # test_initiate_replication()
-    test_fork_termination()
+test_library = {
+    '0': test_actual_update,
+    '1': test_chromosome_replication_default,
+    '2': test_initiate_replication,
+    '3': test_fork_termination,
+}
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='chromosome replication migration')
+    parser.add_argument(
+        '--name', '-n', default=[], nargs='+', help='test ids to run')
+    args = parser.parse_args()
+    run_all = not args.name
+
+    for name in args.name:
+        test_library[name]()
+    if run_all:
+        for name, test in test_library.items():
+            test()
