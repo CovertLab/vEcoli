484197135a0d641d9ed7e02d77ab3a01f6c1bf7a
Eran Agmon <agmon.eran@gmail.com>
2021-06-25 12:01:46 -0700

    move migration to the base directory

--- migration/polypeptide_elongation.py
+++ migration/polypeptide_elongation.py
@@ -1,7 +1,7 @@
 from vivarium.core.engine import Engine, pf
 from ecoli.library.sim_data import LoadSimData
 from ecoli.composites.ecoli_master import SIM_DATA_PATH
-from ecoli.migration.migration_utils import run_ecoli_process
+from migration.migration_utils import run_ecoli_process
 from ecoli.composites.ecoli_master import get_state_from_file
 from ecoli.processes.polypeptide_elongation import PolypeptideElongation
 
