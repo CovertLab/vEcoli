3ad722d49e85551f37dd22ad2932867ffd1df157
thalassemia <67928790+thalassemia@users.noreply.github.com>
2021-06-29 15:39:01 -0700

    Start adding tf_binding migration test

--- ecoli/migration/tf_binding.py
+++ ecoli/migration/tf_binding.py
@@ -1,3 +1,7 @@
+"""
+tests that vivarium-ecoli tf_binding processupdate is the same as saved wcEcoli updates
+"""
+
 from vivarium.core.engine import Engine
 from ecoli.library.sim_data import LoadSimData
 from ecoli.composites.ecoli_master import SIM_DATA_PATH
@@ -10,6 +14,7 @@ load_sim_data = LoadSimData(
             sim_data_path=SIM_DATA_PATH,
             seed=0)
 
+# copy topology from ecoli_master.py, under generate_topology
 TF_BINDING_TOPOLOGY = {
     'promoters': ('unique', 'promoter'),
     'active_tfs': ('bulk',),
@@ -24,6 +29,24 @@ def test_tf_binding_migration():
 
     # run the process and get an update
     actual_update = run_ecoli_process(tf_binding_process, TF_BINDING_TOPOLOGY, total_time=2)
+
+    # separate the update to its ports
+    active_tfs = actual_update['active_tfs']
+    promoters = actual_update['promoters']
+    p_promoter_bound = actual_update['listeners']['rna_synth_prob']['pPromoterBound']
+    n_promoter_bound = actual_update['listeners']['rna_synth_prob']['nPromoterBound']
+    n_actual_bound = actual_update['listeners']['rna_synth_prob']['nActualBound']
+    n_available_promoters = actual_update['listeners']['rna_synth_prob']['n_available_promoters']
+    n_bound_TF_per_TU = actual_update['listeners']['rna_synth_prob']['n_bound_TF_per_TU']
+
+    assert len(p_promoter_bound) == len(promoters), ("Update malformed: number of promoter"
+                                                     "binding probabilities != number of"
+                                                     "promoters")
+
+    assert len(n_promoter_bound) == len(promoters), ("Update malformed: number of promoters"
+                                                     "with binding info != number of"
+                                                     "promoters")
+
 
 
 def run_tf_binding():
@@ -44,9 +67,8 @@ def run_tf_binding():
     experiment.update(10)
 
     data = experiment.emitter.get_data()
-    import ipdb; ipdb.set_trace()
 
 
 if __name__ == "__main__":
-    # test_tf_binding_migration()
-    run_tf_binding()
+    test_tf_binding_migration()
+    # run_tf_binding()
