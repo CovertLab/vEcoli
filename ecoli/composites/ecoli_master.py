5d13e22bd3b89a07bbc57256eb9345d2a74b7867
Eran Agmon <agmon.eran@gmail.com>
2021-06-30 23:40:46 -0700

    get started with api demo notebook

--- ecoli/composites/ecoli_master.py
+++ ecoli/composites/ecoli_master.py
@@ -41,6 +41,133 @@ RAND_MAX = 2**31
 SIM_DATA_PATH = 'reconstruction/sim_data/kb/simData.cPickle'
 
 
+ECOLI_PROCESSES = {
+    'tf_binding': TfBinding,
+    'transcript_initiation': TranscriptInitiation,
+    'transcript_elongation': TranscriptElongation,
+    'rna_degradation': RnaDegradation,
+    'polypeptide_initiation': PolypeptideInitiation,
+    'polypeptide_elongation': PolypeptideElongation,
+    'complexation': Complexation,
+    'two_component_system': TwoComponentSystem,
+    'equilibrium': Equilibrium,
+    'protein_degradation': ProteinDegradation,
+    'metabolism': Metabolism,
+    'chromosome_replication': ChromosomeReplication,
+    'mass': Mass,
+    'divide_condition': DivideCondition,
+}
+
+ECOLI_TOPOLOGY = {
+        'tf_binding': {
+            'promoters': ('unique', 'promoter'),
+            'active_tfs': ('bulk',),
+            'inactive_tfs': ('bulk',),
+            'listeners': ('listeners',)},
+
+        'transcript_initiation': {
+            'environment': ('environment',),
+            'full_chromosomes': ('unique', 'full_chromosome'),
+            'RNAs': ('unique', 'RNA'),
+            'active_RNAPs': ('unique', 'active_RNAP'),
+            'promoters': ('unique', 'promoter'),
+            'molecules': ('bulk',),
+            'listeners': ('listeners',)},
+
+        'transcript_elongation': {
+            'environment': ('environment',),
+            'RNAs': ('unique', 'RNA'),
+            'active_RNAPs': ('unique', 'active_RNAP'),
+            'molecules': ('bulk',),
+            'bulk_RNAs': ('bulk',),
+            'ntps': ('bulk',),
+            'listeners': ('listeners',)},
+
+        'rna_degradation': {
+            'charged_trna': ('bulk',),
+            'bulk_RNAs': ('bulk',),
+            'nmps': ('bulk',),
+            'fragmentMetabolites': ('bulk',),
+            'fragmentBases': ('bulk',),
+            'endoRnases': ('bulk',),
+            'exoRnases': ('bulk',),
+            'subunits': ('bulk',),
+            'molecules': ('bulk',),
+            'RNAs': ('unique', 'RNA'),
+            'active_ribosome': ('unique', 'active_ribosome'),
+            'listeners': ('listeners',)},
+
+        'polypeptide_initiation': {
+            'environment': ('environment',),
+            'listeners': ('listeners',),
+            'active_ribosome': ('unique', 'active_ribosome'),
+            'RNA': ('unique', 'RNA'),
+            'subunits': ('bulk',)},
+
+        'polypeptide_elongation': {
+            'environment': ('environment',),
+            'listeners': ('listeners',),
+            'active_ribosome': ('unique', 'active_ribosome'),
+            'molecules': ('bulk',),
+            'monomers': ('bulk',),
+            'amino_acids': ('bulk',),
+            'ppgpp_reaction_metabolites': ('bulk',),
+            'uncharged_trna': ('bulk',),
+            'charged_trna': ('bulk',),
+            'charging_molecules': ('bulk',),
+            'synthetases': ('bulk',),
+            'subunits': ('bulk',),
+            'polypeptide_elongation': ('process_state', 'polypeptide_elongation')},
+
+        'complexation': {
+            'molecules': ('bulk',)},
+
+        'two_component_system': {
+            'listeners': ('listeners',),
+            'molecules': ('bulk',)},
+
+        'equilibrium': {
+            'listeners': ('listeners',),
+            'molecules': ('bulk',)},
+
+        'protein_degradation': {
+            'metabolites': ('bulk',),
+            'proteins': ('bulk',)},
+
+        'metabolism': {
+            'metabolites': ('bulk',),
+            'catalysts': ('bulk',),
+            'kinetics_enzymes': ('bulk',),
+            'kinetics_substrates': ('bulk',),
+            'amino_acids': ('bulk',),
+            'listeners': ('listeners',),
+            'environment': ('environment',),
+            'polypeptide_elongation': ('process_state', 'polypeptide_elongation')},
+
+        'chromosome_replication': {
+            'replisome_trimers': ('bulk',),
+            'replisome_monomers': ('bulk',),
+            'dntps': ('bulk',),
+            'ppi': ('bulk',),
+            'active_replisomes': ('unique', 'active_replisome',),
+            'oriCs': ('unique', 'oriC',),
+            'chromosome_domains': ('unique', 'chromosome_domain',),
+            'full_chromosomes': ('unique', 'full_chromosome',),
+            'listeners': ('listeners',),
+            'environment': ('environment',)},
+
+        'mass': {
+            'bulk': ('bulk',),
+            'unique': ('unique',),
+            'listeners': ('listeners',)},
+
+        'divide_condition': {
+            'variable': ('listeners', 'mass', 'cell_mass'),
+            'divide': ('globals', 'divide',),
+        },
+    }
+
+
 class Ecoli(Composer):
 
     defaults = {
