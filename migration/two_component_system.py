82c36d24ad746a0808532c8b3c3e80e22c08d2fd
Abhi Kumar <abhinavkumar@DNa809ed3.SUNet>
2021-07-01 15:39:21 -0700

    clean up migration/two_component_system.py

--- migration/two_component_system.py
+++ migration/two_component_system.py
@@ -1,10 +1,7 @@
-from vivarium.core.engine import Engine
 from ecoli.library.sim_data import LoadSimData
 from ecoli.composites.ecoli_master import SIM_DATA_PATH
 from ecoli.processes.two_component_system import TwoComponentSystem
 from migration.migration_utils import run_ecoli_process
-from ecoli.composites.ecoli_master import get_state_from_file
-
 
 load_sim_data = LoadSimData(
             sim_data_path=SIM_DATA_PATH,
@@ -14,43 +11,16 @@ TOPOLOGY = {
         'listeners': ('listeners',),
         'molecules': ('bulk',)}
 
-
 def test_two_component_system_migration():
     # Create process, experiment, loading in initial state from file.
     config = load_sim_data.get_two_component_system_config()
     two_component_system_process = TwoComponentSystem(config)
 
     # run the process and get an update
-    actual_update = run_ecoli_process(two_component_system_process, TOPOLOGY, total_time=2)
+    actual_update = run_ecoli_process(two_component_system_process, TOPOLOGY, total_time=2, initial_time=1000)
 
-
-def test_two_component_system():
-    # Create process, experiment, loading in initial state from file.
-    config = load_sim_data.get_two_component_system_config()
-    two_component_system_process = TwoComponentSystem(config)
-
-    initial_state = get_state_from_file(
-        path=f'data/wcecoli_t0.json')
-
-    two_component_system_composite = two_component_system_process.generate()
-
-    experiment = Engine({
-        'processes': two_component_system_composite['processes'],
-        'topology': {two_component_system_process.name: TOPOLOGY},
-        'initial_state': initial_state
-    })
-
-    experiment.update(10)
-
-    data = experiment.emitter.get_data()
-
-    return data
-
-def run_two_component_system():
-    data = test_two_component_system()
+    #ipdb breakpoint - can import in the middle!
     import ipdb; ipdb.set_trace()
 
-
 if __name__ == "__main__":
-    # test_two_component_system_migration()
-    run_two_component_system()
+    test_two_component_system_migration()
