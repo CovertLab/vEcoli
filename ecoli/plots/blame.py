e83952481e6e551a3dde92177ac68118d7f2cdff
Michael <michaelyang42@gmail.com>
2021-07-02 20:40:17 -0400

    basic blame plot functionality working

--- ecoli/plots/blame.py
+++ ecoli/plots/blame.py
@@ -2,18 +2,94 @@ import os
 import json
 import unum
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import coo_matrix
 from matplotlib import pyplot as plt
+import matplotlib.colors as colors
 
 from collections import Counter
 
 from ecoli.composites.ecoli_master import ECOLI_TOPOLOGY
 
+
+# set the colormap and centre the colorbar
+class MidpointNormalize(colors.Normalize):
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+
+
 def blame_plot(data, filename='out/ecoli_master/blame.png'):
-    data = extract_bulk(data)
+    if 'log_update' not in data.keys():
+        raise ValueError("Missing log_update in data; did you run vivarium-ecoli without -blame?")
+
+    bulk_idx, process_idx, plot_data = extract_bulk(data)
+    plot_data = plot_data.toarray()
+
+    # exclude zero-change molecules
+    nonzero_mols = np.sum(plot_data, axis=1) != 0
+    bulk_idx = bulk_idx[nonzero_mols]
+    plot_data = plot_data[nonzero_mols, :]
+
+    # sort molecules by sum of absolute changes
+    sorted_mol_idx = np.argsort(np.sum(np.abs(plot_data), axis=1))
+    bulk_idx = bulk_idx[sorted_mol_idx]
+    plot_data = plot_data[sorted_mol_idx, :]
+
+    # normalize for each process
+    # col_sums = np.sum(plot_data, axis=0)
+    # col_sums[col_sums == 0] = 1
+    # plot_data = plot_data / col_sums
+    # min_val = np.min(plot_data)
+    # max_val = np.max(plot_data)
+
+    # normalize for each molecule
+    row_sums = plot_data.sum(axis=1)
+    row_sums[row_sums == 0] = 1
+    plot_data = (plot_data.T / row_sums).T
+    min_val = np.min(plot_data)
+    max_val = np.max(plot_data)
 
     fig, ax = plt.subplots()
-    im = ax.imshow(data)
+    fig.set_size_inches(2 * plot_data.shape[1], plot_data.shape[0] / 10)
+    im = ax.imshow(plot_data,
+                   aspect='auto',
+                   cmap=plt.get_cmap('seismic'),
+                   clim=(min_val, max_val),
+                   norm=MidpointNormalize(midpoint=0, vmin=min_val, vmax=max_val))
+
+    # show and rename ticks
+    ax.set_xticks(np.arange(plot_data.shape[1]))
+    ax.set_yticks(np.arange(plot_data.shape[0]))
+    ax.set_xticklabels(process_idx)
+    ax.set_yticklabels(bulk_idx)
+
+    # Rotate the tick labels and set their alignment.
+    # plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+    #          rotation_mode="anchor")
+
+    ax.xaxis.tick_top()
+    ax.xaxis.set_label_position('top')
+
+    # # Loop over data dimensions and create text annotations.
+    # for i in range(len(vegetables)):
+    #     for j in range(len(farmers)):
+    #         text = ax.text(j, i, harvest[i, j],
+    #                        ha="center", va="center", color="w")
+    #
+    # ax.set_title("Harvest of local farmers (in tons/year)")
+    fig.tight_layout()
 
     plt.savefig(filename)
 
@@ -40,29 +116,45 @@ def extract_bulk(data):
             if port not in bulk_processes[process]:
                 break
 
-            port_data = {k : np.sum(v) for k, v in updates[port].items()}
+            port_data = {k: np.sum(v) for k, v in updates[port].items()}
             process_data.update(port_data)
 
         collected_data[process] = dict(process_data)
 
     # convert dictionary to array
-    bulk_indices = []
-    for process, data in collected_data.items():
-        bulk_indices += list(data.keys())
-    bulk_indices = np.array(bulk_indices)
+    collected_data = {process: idx_array_from(data) for process, data in collected_data.items()}
 
-    row = []  # processes
-    col = np.array([])  # molecules
-    data_out = []
-    for i, (process, data) in enumerate(collected_data.items()):
-        cols = np.where(np.isin(bulk_indices, np.array(data.keys())))
-        col = np.concatenate(col, cols)
-        row += [i for x in range(len(cols))]
-        data_out.append(data.values())
+    bulk_indices = set()
+    for idx, _ in collected_data.values():
+        bulk_indices.update(idx)
+    bulk_indices = np.array(list(bulk_indices))
 
-        import ipdb; ipdb.set_trace()
+    col_i = 0
+    row = []  # molecules
+    col = []  # processes
+    data_out = []  # update data
+    process_idx = []
+    for process, (idx, value) in collected_data.items():
+        rows = np.where(np.isin(bulk_indices, idx))[0]
+        if rows.size != 0:
+            row = np.concatenate((row, rows))
+            col += [col_i for _ in range(len(rows))]
+            data_out = np.concatenate((data_out, value))
+            process_idx.append(process)
+            col_i += 1
 
-    return csr_matrix([data_out, [row, col]])
+    bulk_data = coo_matrix((data_out, (row.astype(int), np.array(col))))
+    return bulk_indices, process_idx, bulk_data
+
+
+def idx_array_from(dictionary):
+    idx = []
+    values = []
+    for k, v in dictionary.items():
+        idx.append(k)
+        values.append(v)
+
+    return np.array(idx), np.array(values)
 
 
 def write_json(path, numpy_dict):
@@ -90,6 +182,7 @@ def write_json(path, numpy_dict):
     with open(path, 'w') as outfile:
         json.dump(numpy_dict, outfile, cls=NpEncoder)
 
+
 def test_blame():
     try:
         with open("data/blame_test_data.json") as f:
@@ -102,6 +195,5 @@ def test_blame():
     blame_plot(data, 'out/ecoli_master/blame_test.png')
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     test_blame()
