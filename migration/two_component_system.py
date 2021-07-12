2adb6ebdb743935953d35d3857b88f33130d01f6
Abhi Kumar <abhinavkumar@DNa809ed3.SUNet>
2021-07-12 11:27:51 -0700

    test stoich matricies are same

--- migration/two_component_system.py
+++ migration/two_component_system.py
@@ -11,6 +11,7 @@ load_sim_data = LoadSimData(
 
 def test_two_component_system_migration():
     # Create process, experiment, loading in initial state from file.
+    #config, stoichI, stoichJ, stoichV = load_sim_data.get_two_component_system_config()
     config = load_sim_data.get_two_component_system_config()
     two_component_system_process = TwoComponentSystem(config)
 
@@ -19,24 +20,29 @@ def test_two_component_system_migration():
         'molecules': ('bulk',)}
 
     # run the process and get an update
-    actual_update = run_ecoli_process(two_component_system_process, topology, total_time=2, initial_time=0)
-    """
+    #actual_update = run_ecoli_process(two_component_system_process, topology, total_time=2, initial_time=10)
+
     actual_updates = {}
     for x in range(10):
         config = load_sim_data.get_two_component_system_config(random_seed=x)
         two_component_system_process = TwoComponentSystem(config)
 
-        actual_update = run_ecoli_process(two_component_system_process, topology, total_time=2, initial_time=0)
+        actual_update = run_ecoli_process(two_component_system_process, topology, total_time=2, initial_time=10)
         actual_updates[x] = actual_update
-    """
+    import ipdb; ipdb.set_trace()
+
     with open("data/two_component_system_update_t2.json") as f:
         wc_update = json.load(f)
 
-    # plot all the actual_updates and compare plots
-    # save the stoichiometry matrix and compare
+    # TODO: Set seed the same between vivarium and wcecoi and test actual_update vs wc_update
+        # issue 1 - where is the seed in wcEcoli?
+        # issue 2 - setting seed this way in vivarium does not change the values in actual_update
+    # TODO: plot all the actual_updates and compare plots
+        # issue 1 - actual_updates are not different from another because seed doesn't change actual_update values
+    # TODO: save the stoichiometry matrix and compare
+        # finding 1 - stoich matricies are the same!
 
-    # #ipdb breakpoint - can import in the middle!
-    import ipdb; ipdb.set_trace()
+    #import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
     test_two_component_system_migration()
