549155fe56672886f446f6e9c8df5abb1c43b093
Michael <michaelyang42@gmail.com>
2021-07-26 19:34:00 -0400

    add name to exchange stub

--- ecoli/processes/exchange_stub.py
+++ ecoli/processes/exchange_stub.py
@@ -8,6 +8,7 @@ from vivarium.core.composition import simulate_process
 
 
 class Exchange(Process):
+    name = 'exchange'
     defaults = {'exchanges': {}}
 
     def __init__(self, parameters=None):
