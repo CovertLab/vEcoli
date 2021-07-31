82c7df19fc9a7e639f04ebf56e4a6f9c5cc80c23
Sean Cheah <cheah_sean@yahoo.com>
2021-07-30 22:38:50 -0700

    Making tf_binding migration test exact

--- data/ecoli_master_configs/default.py
+++ data/ecoli_master_configs/default.py
@@ -35,7 +35,9 @@ ECOLI_TOPOLOGY = {
             'promoters': ('unique', 'promoter'),
             'active_tfs': ('bulk',),
             'inactive_tfs': ('bulk',),
-            'listeners': ('listeners',)},
+            'listeners': ('listeners',),
+            # Non-partitioned counts
+            'active_tfs_total': ('bulk',),},
 
         'transcript_initiation': {
             'environment': ('environment',),
@@ -89,7 +91,12 @@ ECOLI_TOPOLOGY = {
             'charging_molecules': ('bulk',),
             'synthetases': ('bulk',),
             'subunits': ('bulk',),
-            'polypeptide_elongation': ('process_state', 'polypeptide_elongation')},
+            'polypeptide_elongation': ('process_state', 'polypeptide_elongation'),
+            # Non-partitioned counts
+            'molecules_total': ('bulk',),
+            'amino_acids_total': ('bulk',),
+            'charged_trna_total': ('bulk',),
+            'charging_molecules_total': ('bulk',),},
 
         'complexation': {
             'molecules': ('bulk',)},
