cce092914fae32f77dc1718eb6c32974cb98f268
Sean Cheah <cheah_sean@yahoo.com>
2021-08-05 11:52:07 -0700

    Minor bug fixes

--- migration/dict_compare.py
+++ migration/dict_compare.py
@@ -24,21 +24,23 @@ def recursive_compare(d1, d2, level='root'):
         for i in range(common_len):
             recursive_compare(d1[i], d2[i], level='{}[{}]'.format(level, i))
             
-    elif isinstance(d1, np.ndarray) and isinstance(d2, np.ndarray):
-        if d1.shape != d2.shape:
-            print('{:<20} shape1={}; shape2={}'.format(level, d1.shape, d2.shape))
-        common_rows = min(d1.shape[0], d2.shape[0])
-        for i in range(common_rows):
-            recursive_compare(d1[i], d2[i], level='{}[{}]'.format(level, i))
+    elif isinstance(d1, np.ndarray) or isinstance(d2, np.ndarray):
+        recursive_compare(list(d1), list(d2), level='{}'.format(level))
     
     elif isinstance(d1, Unum) and isinstance(d2, Unum):
-        if len(d1) != len(d2):
-            print('{:<20} len1={}; len2={}'.format(level, len(d1), len(d2)))
+        recursive_compare(d1.asNumber(), d2.asNumber(), level='{}'.format(level))
     
     elif isinstance(d1, numbers.Number) and isinstance(d2, numbers.Number):
-        if percent_error(d1, d2) >= 0.05:
+        if not np.isclose(d1, d2, rtol=0.01, atol=1):
             print('{:<20} {} != {}'.format(level, d1, d2))
+            
+    elif isinstance(d1, set) and isinstance(d2, set):
+        if len(d1 - d2) != 0:
+            print('{:<20} {} in 1st but not 2nd set'.format(level, d1 - d2))
     
     else:
-        if d1 != d2:
+        try:
+            if d1 != d2:
+                print('{:<20} {} != {}'.format(level, d1, d2))
+        except ValueError:
             print('{:<20} {} != {}'.format(level, d1, d2))
