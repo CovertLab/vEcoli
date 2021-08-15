bc8ecab82c2c71b882d2fc5f276b2df745a85e95
Eran Agmon <agmon.eran@gmail.com>
2021-08-15 11:58:43 -0700

    bring back run_ecoli for testing ecoli_master

--- ecoli/composites/ecoli_master.py
+++ ecoli/composites/ecoli_master.py
@@ -9,7 +9,6 @@ import argparse
 
 from vivarium.core.composer import Composer
 from vivarium.core.engine import pp, Engine
-from vivarium.plots.topology import plot_topology
 from vivarium.library.topology import assoc_path
 from vivarium.library.dict_utils import deep_merge
 
@@ -26,7 +25,9 @@ from ecoli.plots.topology import get_ecoli_master_topology_settings
 # state
 from ecoli.states.wcecoli_state import get_state_from_file
 
-from ecoli.library.data_predicates import all_nonnegative
+# plotting
+from ecoli.plots.blame import blame_plot
+from vivarium.plots.topology import plot_topology
 
 
 RAND_MAX = 2**31
@@ -128,6 +129,93 @@ class Ecoli(Composer):
         return topology
 
 
+def run_ecoli(
+        total_time=10,
+        config=None,
+        divide=False,
+        progress_bar=True,
+        blame=False,
+        time_series=True
+):
+    """Run ecoli_master simulations
+    Arguments:
+        * **total_time** (:py:class:`int`): the total runtime of the experiment
+        * **config** (:py:class:`dict`):
+    Returns:
+        * output data
+    """
+    # make the ecoli config dictionary
+    agent_id = '0'
+    ecoli_config = {
+        'blame': blame,
+        'agent_id': agent_id,
+        # TODO -- remove schema override once values don't go negative
+        '_schema': {
+            'equilibrium': {
+                'molecules': {
+                    'PD00413[c]': {'_updater': 'nonnegative_accumulate'}
+                }
+            },
+        },
+    }
+    if config:
+        ecoli_config = deep_merge(ecoli_config, config)
+
+    # initialize the ecoli composer
+    ecoli_composer = Ecoli(ecoli_config)
+
+    # set path at which agent is initialized
+    path = tuple()
+    if divide:
+        path = ('agents', agent_id,)
+
+    # get initial state
+    initial_state = ecoli_composer.initial_state(path=path)
+
+    # generate the composite at the path
+    ecoli = ecoli_composer.generate(path=path)
+
+    # make the experiment
+    ecoli_experiment = Engine(**{
+        'processes': ecoli.processes,
+        'topology': ecoli.topology,
+        'initial_state': initial_state,
+        'progress_bar': progress_bar,
+        'emit_config': False,
+        # Not emitting every step is faster but breaks blame.py
+        #'emit_step': 1000,
+        #'emitter': 'database'
+    })
+
+    # run the experiment
+    ecoli_experiment.update(total_time)
+
+    # retrieve the data
+    if time_series:
+        output = ecoli_experiment.emitter.get_timeseries()
+    else:
+        output = ecoli_experiment.emitter.get_data()
+
+    return output
+
+
+def test_division():
+    """
+    Work in progress to get division working
+    * TODO -- unique molecules need to be divided between daughter cells!!! This can get sophisticated
+    """
+
+    config = {
+        'division': {
+            'threshold': 1170}}
+    output = run_ecoli(
+        total_time=10,
+        divide=True,
+        config=config,
+        progress_bar=False,
+    )
+
+
 def ecoli_topology_plot(config={}, filename=None, out_dir=None):
     """Make a topology plot of Ecoli"""
     agent_id_config = {'agent_id': '1'}
@@ -141,8 +229,40 @@ def ecoli_topology_plot(config={}, filename=None, out_dir=None):
     return topo_plot
 
 
+test_library = {
+    '0': run_ecoli,
+    '1': test_division,
+}
+
+
 def main():
-    pass
+    out_dir = os.path.join('out', 'ecoli_master')
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    parser = argparse.ArgumentParser(description='ecoli_master')
+    parser.add_argument(
+        '--name', '-n', default=[], nargs='+',
+        help='test ids to run')
+    parser.add_argument(
+        '--topology', '-t', action='store_true', default=False,
+        help='save a topology plot of ecoli master')
+    parser.add_argument(
+        '--blame', '-b', action='store_true', default=False,
+        help='when running simulation, create a report of which processes affected which molecules')
+    args = parser.parse_args()
+
+    if args.topology:
+        ecoli_topology_plot(filename='ecoli_master', out_dir=out_dir)
+    elif args.name:
+        for name in args.name:
+            test_library[name]()
+    else:
+        output = run_ecoli(
+            blame=args.blame,
+        )
+        if args.blame:
+            blame_plot(output, highlight_molecules=['PD00413[c]'])
 
 
 if __name__ == '__main__':
