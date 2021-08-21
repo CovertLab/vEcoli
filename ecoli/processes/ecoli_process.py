fd0e6883593c346dce27323d206d26780b3b4abe
Eran Agmon <agmon.eran@gmail.com>
2021-08-21 10:49:49 -0700

    begin work on a EcoliProcess subclass, which does the topology registration.

---
+++ ecoli/processes/ecoli_process.py
@@ -0,0 +1,37 @@
+
+import abc
+
+from vivarium.core.process import Process
+from vivarium.library.dict_utils import deep_merge
+from ecoli.processes.registries import topology_registry
+
+
+class EcoliProcess(Process):
+    name = None
+    topology = None
+    defaults = {}
+
+    def __init__(self, parameters=None):
+        super().__init__(parameters)
+        assert self.name
+        assert self.topology
+
+        topology_registry.register(self.name, self.topology)
+
+    @abc.abstractmethod
+    def ports_schema(self):
+        return {}
+
+    @abc.abstractmethod
+    def calculate_request(self, timestep, states):
+        return {}
+
+    @abc.abstractmethod
+    def evolve_state(self, timestep, states):
+        return {}
+
+    def next_update(self, timestep, states):
+        requests = self.calculate_request(timestep, states)
+        states = deep_merge(states, requests)
+        update = self.evolve_state(timestep, states)
+        return update
