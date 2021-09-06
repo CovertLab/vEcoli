397c3c545c4b6ed7375723a8caf3ed5a68f30450
Eran Agmon <agmon.eran@gmail.com>
2021-09-06 12:41:25 -0700

    make PartitionedProcess, use it to implement Complexation

--- ecoli/processes/partition.py
+++ ecoli/processes/partition.py
@@ -5,9 +5,11 @@ Includes Requester and Evolver processes, which take an EcoliProcess
 and use its calculate_request or evolve_state methods for the Process
 next_update.
 """
+import abc
 
 from vivarium.core.process import Deriver, Process
 from vivarium.library.dict_utils import deep_merge
+from ecoli.processes.registries import topology_registry
 
 
 def change_bulk_updater(schema, new_updater):
@@ -135,3 +137,44 @@ class Evolver(Process):
     def next_update(self, timestep, states):
         states = deep_merge(states, states.pop('allocate'))
         return self.process.evolve_state(timestep, states)
+
+
+class PartitionedProcess(Process):
+    name = None
+    topology = None
+    defaults = {
+        'evolve_only': False,
+        'request_only': False,
+    }
+
+    def __init__(self, parameters=None):
+        super().__init__(parameters)
+        assert self.name
+        assert self.topology
+
+        topology_registry.register(self.name, self.topology)
+
+    @abc.abstractmethod
+    def ports_schema(self):
+        return {}
+
+    @abc.abstractmethod
+    def calculate_request(self, timestep, states):
+        return {}
+
+    @abc.abstractmethod
+    def evolve_state(self, timestep, states):
+        return {}
+
+    def next_update(self, timestep, states):
+
+        if self.parameters['request_only']:
+            return self.calculate_request(timestep, states)
+        elif self.parameters['evolve_only']:
+            return self.evolve_state(timestep, states)
+
+        requests = self.calculate_request(timestep, states)
+        states = deep_merge(states, requests)
+        update = self.evolve_state(timestep, states)
+
+        return update
