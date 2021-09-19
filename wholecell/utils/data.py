22b8cdbfcc9acc517db43d764d52d3482deb462b
matt-wolff <mbwolff@stanford.edu>
2021-09-18 18:10:48 -0700

    Added new create_unique_indexes function.

--- wholecell/utils/data.py
+++ wholecell/utils/data.py
@@ -8,6 +8,15 @@ import os
 from typing import Any, Dict, Iterable, Mapping
 
 from six import string_types
+from uuid import uuid4
+
+
+def create_unique_indexes(n_indexes):
+	"""
+	Creates a list by concatenating two strings formed using uuid4()
+	to avoid generating the same index twice.
+	"""
+	return [str(int(str(uuid4())[0:7], 16)) + str(int(str(uuid4())[0:7], 16)) for i in range(n_indexes)]
 
 
 def dissoc(mapping, keys):
