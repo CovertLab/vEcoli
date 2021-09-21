d54dc7e2249aa262babde28e1ba14f3f3d7aea9e
Eran Agmon <agmon.eran@gmail.com>
2021-09-21 16:20:29 -0700

    add chemotaxis processes

---
+++ ecoli/composites/chemotaxis_minimal.py
@@ -0,0 +1,131 @@
+"""
+==============================
+Chemotaxis Minimal Composite
+==============================
+"""
+
+import os
+
+from vivarium.core.composer import Composer
+from vivarium.core.composition import COMPOSITE_OUT_DIR, simulate_composite
+from vivarium.plots.simulation_output import plot_simulation_output
+
+# processes
+from ecoli.processes.chemotaxis.chemoreceptor_cluster import (
+    ReceptorCluster,
+    get_exponential_random_timeline,
+)
+from ecoli.processes.chemotaxis.coarse_motor import MotorActivity
+
+
+
+NAME = 'chemotaxis_minimal'
+
+
+class ChemotaxisMinimal(Composer):
+    """ Chemotaxis Minimal Composite
+
+     A chemotactic cell with only receptor and motor processes.
+     """
+
+    name = NAME
+    defaults = {
+        'ligand_id': 'MeAsp',
+        'initial_ligand': 0.1,
+        'boundary_path': ('boundary',),
+        'receptor': {},
+        'motor': {},
+    }
+
+    def __init__(self, config):
+        super(ChemotaxisMinimal, self).__init__(config)
+
+    def generate_processes(self, config):
+
+        receptor_config = config['receptor']
+        motor_config = config['motor']
+
+        ligand_id = config['ligand_id']
+        initial_ligand = config['initial_ligand']
+        receptor_config.update({
+            'ligand_id': ligand_id,
+            'initial_ligand': initial_ligand})
+
+        # declare the processes
+        receptor = ReceptorCluster(receptor_config)
+        motor = MotorActivity(motor_config)
+
+        return {
+            'receptor': receptor,
+            'motor': motor}
+
+    def generate_topology(self, config):
+        boundary_path = config['boundary_path']
+        external_path = boundary_path + ('external',)
+        return {
+            'receptor': {
+                'external': external_path,
+                'internal': ('cell',)},
+            'motor': {
+                'external': boundary_path,
+                'internal': ('cell',)}}
+
+
+
+def test_chemotaxis_minimal(total_time=10):
+    environment_port = ('external',)
+    ligand_id = 'MeAsp'
+    initial_conc = 0
+    time_step = 0.1
+
+    # make the compartment
+    compartment_config = {
+        'external_path': (environment_port,),
+        'ligand_id': ligand_id,
+        'initial_ligand': initial_conc}
+    composite = ChemotaxisMinimal(compartment_config).generate()
+
+    # configure timeline
+    exponential_random_config = {
+        'ligand': ligand_id,
+        'environment_port': environment_port,
+        'time': total_time,
+        'timestep': time_step,
+        'initial_conc': initial_conc,
+        'base': 1 + 4e-4,
+        'speed': 14,
+    }
+    timeline = get_exponential_random_timeline(exponential_random_config)
+
+    # run experiment
+    experiment_settings = {
+        'timeline': {
+            'timeline': timeline,
+            'paths': {'external': ('boundary', 'external')}},
+        'timestep': time_step,
+        'total_time': total_time}
+    timeseries = simulate_composite(composite, experiment_settings)
+
+    return timeseries
+
+
+if __name__ == '__main__':
+    out_dir = os.path.join(COMPOSITE_OUT_DIR, NAME)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # run the composite
+    timeseries = test_chemotaxis_minimal(total_time=60)
+
+    # plot
+    plot_settings = {
+        'max_rows': 20,
+        'remove_zeros': True,
+        'overlay': {
+            'reactions': 'flux'},
+        'skip_ports': ['prior_state', 'null', 'global']}
+    plot_simulation_output(
+        timeseries,
+        plot_settings,
+        out_dir,
+        'exponential_timeline')
---
+++ ecoli/processes/chemotaxis/membrane_potential.py
@@ -0,0 +1,212 @@
+"""
+===========================
+Membrane Potential
+===========================
+"""
+
+import os
+
+import numpy as np
+import scipy.constants as constants
+
+from vivarium.core.process import Process
+from vivarium.core.composition import (
+    simulate_process,
+    PROCESS_OUT_DIR,
+)
+from vivarium.plots.simulation_output import plot_simulation_output
+
+
+NAME = 'membrane_potential'
+
+
+class NoChargeError(Exception):
+    pass
+
+
+class MembranePotential(Process):
+    """ Membrane Potential
+
+    :term:`Ports`:
+    * ``internal``: holds the concentrations of internal ions
+    * ``external``: holds the concentrations of external ions
+    * ``membrane``: holds the cross-membrane properties 'PMF', 'd_V', 'd_pH'
+
+    Notes:
+        * PMF ~170mV at pH 7. ~140mV at pH 7.7 (Berg)
+        * Ecoli internal pH in range 7.6-7.8 (Berg)
+
+        * (mmol) http://book.bionumbers.org/what-are-the-concentrations-of-different-ions-in-cells/
+        * Schultz, Stanley G., and A. K. Solomon. "Cation Transport in Escherichia coli" (1961)
+        * Zilberstein, Dan, et al. "Escherichia coli intracellular pH, membrane potential, and cell growth."
+          Journal of bacteriology 158.1 (1984): 246-252.
+        * TODO -- add Mg2+, Ca2+
+    """
+
+    name = NAME
+    defaults = {
+
+        'initial_state': {
+            'internal': {
+                'K': 300,  # (mmol) 30-300
+                'Na': 10,  # (mmol) 10
+                'Cl': 10,
+                'PROTON': 0},  # (mmol) 10-200 media-dependent
+            'external': {
+                'K': 5,
+                'Na': 145,
+                'Cl': 110,  # (mmol)
+                'PROTON': 0,
+                'T': 310.15}
+        },
+
+        'permeability_map': {
+            'K': 'p_K',
+            'Na': 'p_Na',
+            'Cl': 'p_Cl',
+            'PROTON': 'p_PROTON',
+        },
+
+        # cation is positively charged, anion is negatively charged
+        'charge_map': {
+            'K': 'cation',
+            'Na': 'cation',
+            'Cl': 'anion',
+            'PROTON': 'cation',
+        },
+
+        # parameters
+        'p_K': 1,  # unitless, relative membrane permeability of K
+        'p_Na': 0.05,  # unitless, relative membrane permeability of Na
+        'p_Cl': 0.05,  # unitless, relative membrane permeability of Cl
+        'p_PROTON': 1,  # should be more?
+
+        # physical constants
+        'R': constants.gas_constant,  # (J * K^-1 * mol^-1) gas constant
+        'F': constants.physical_constants['Faraday constant'][0],  # (C * mol^-1) Faraday constant
+        'k': constants.Boltzmann,  # (J * K^-1) Boltzmann constant
+        'e': 1,  # proton charge
+    }
+
+    def __init__(self, parameters=None):
+        super(MembranePotential, self).__init__(parameters)
+
+    def ports_schema(self):
+        ports = [
+            'internal',
+            'membrane',
+            'external',
+        ]
+        schema = {port: {} for port in ports}
+
+        ## internal
+        # internal ions and charge (c_in)
+        for state_id, value in self.parameters['initial_state']['internal'].items():
+            schema['internal'][state_id] = {
+                '_default': value,
+                '_emit': True,
+            }
+
+        ## external
+        # external ions, charge (c_out) and temperature (T)
+        for state_id, value in self.parameters['initial_state']['external'].items():
+            schema['external'][state_id] = {
+                '_default': value,
+                '_emit': True,
+            }
+
+        ## membrane
+        # proton motive force (PMF), electrical difference (d_V), pH difference (d_pH)
+        for state in ['PMF', 'd_V', 'd_pH']:
+            schema['membrane'][state] = {
+                '_default': 0.0,
+                '_updater': 'set',
+                '_emit': True,
+            }
+        return schema
+
+    def next_update(self, timestep, states):
+        internal_state = states['internal']
+        external_state = states['external']
+
+        # parameters
+        R = self.parameters['R']
+        F = self.parameters['F']
+        k = self.parameters['k']
+        e = self.parameters['e']
+
+        # state
+        T = external_state['T']  # temperature
+
+        # Membrane potential.
+        numerator = 0
+        denominator = 0
+        for ion_id, p_ion_id in self.parameters['permeability_map'].items():
+            charge = self.parameters['charge_map'][ion_id]
+            p_ion = self.parameters[p_ion_id]
+
+            # ions states
+            internal = internal_state[ion_id]
+            external = external_state[ion_id]
+
+            if charge == 'cation':
+                numerator += p_ion * external
+                denominator += p_ion * internal
+            elif charge == 'anion':
+                numerator += p_ion * internal
+                denominator += p_ion * external
+            else:
+                raise NoChargeError(
+                    "No charge given for {}".format(ion_id))
+
+        # Goldman equation for membrane potential
+        # expected d_V = -120 mV
+        d_V = (R * T) / F * np.log(numerator / denominator) * 1e3  # (mV). 1e3 factor converts from V
+
+        # transmembrane pH difference
+        # -2.3 * k * T / e  # -2.3 Boltzmann constant * temperature
+        # expected d_pH = -50 mV
+        d_pH = -50  # (mV) for cells grown at pH 7. (Berg, H. "E. coli in motion", pg 105)
+
+        # proton motive force
+        PMF = d_V + d_pH
+
+        return {
+            'membrane': {
+                'd_V': d_V,
+                'd_pH': d_pH,
+                'PMF': PMF}}
+
+
+def test_mem_potential():
+    # configure process
+    parameters = {}
+    mp = MembranePotential(parameters)
+    timeline = [
+        (0, {('external', 'Na'): 1}),
+        (50, {('external', 'Na'): 10}),
+        (100, {})]
+
+    settings = {'timeline': {'timeline': timeline}}
+    timeseries = simulate_process(mp, settings)
+
+    PMF_timeseries = timeseries['membrane']['PMF']
+    assert PMF_timeseries[-1] > PMF_timeseries[2]
+    return timeseries
+
+
+def main():
+    out_dir = os.path.join(PROCESS_OUT_DIR, NAME)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    timeseries = test_mem_potential()
+    settings = {
+        'remove_first_timestep': True
+    }
+    plot_simulation_output(timeseries, settings, out_dir)
+
+
+
+if __name__ == '__main__':
+    main()
