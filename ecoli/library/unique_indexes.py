d2821e14f7a7effdf64fc736136ab3c87dcbe8df
matt-wolff <mbwolff@stanford.edu>
2021-09-20 22:12:19 -0700

    Generates indexes through one call to uuid4().

--- ecoli/library/unique_indexes.py
+++ ecoli/library/unique_indexes.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 def create_unique_indexes(n_indexes):
 	"""
-	Creates a list by concatenating two strings formed using uuid4()
-	to avoid generating the same index twice.
+	Creates a list of unique indexes by using uuid4() to generate each index.
 	"""
-	return [str(int(str(uuid4())[0:7], 16)) + str(int(str(uuid4())[0:7], 16)) for i in range(n_indexes)]
+	return [str(uuid4().int)[-14:] for i in range(n_indexes)]
+
