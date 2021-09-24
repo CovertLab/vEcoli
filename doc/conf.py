b1540574753eba275d4ff0f2a84aa5652fc795e4
cs <cs.temporary@icloud.com>
2021-09-24 13:10:05 +0000

    Fix AttributeError by not mocking six

--- doc/conf.py
+++ doc/conf.py
@@ -91,7 +91,7 @@ intersphinx_mapping = {
 autodoc_inherit_docstrings = False
 # The Python dependencies aren't really required for building the docs
 autodoc_mock_imports = [
-    'cobra', 'arrow', 'IPython', 'six', 'numba', 'line-profiler',
+    'cobra', 'arrow', 'IPython', 'numba', 'line-profiler',
     'matplotlib', 'sympy', 'iteround', 'vivarium_multibody', 'pytest',
     # Runs code on import and fails due to missing solvers.
     'wholecell.utils.modular_fba',
