5cae05c4b063b3a2e17ed564d204c237f473d619
matt-wolff <mbwolff@stanford.edu>
2021-10-04 21:04:28 -0700

    Fixed the mapping in transcript_elongation.

--- ecoli/library/unique_indexes.py
+++ ecoli/library/unique_indexes.py
@@ -5,5 +5,5 @@ def create_unique_indexes(n_indexes):
 	"""
 	Creates a list of unique indexes by using uuid4() to generate each index.
 	"""
-	return [str(uuid4().int)[-14:] for i in range(n_indexes)]
+	return [int(str(uuid4().int)[-14:]) for i in range(n_indexes)]
 
