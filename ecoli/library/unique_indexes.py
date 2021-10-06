8288a8ce7aa191a8b3855a30867ddb134452dd9b
matt-wolff <mbwolff@stanford.edu>
2021-10-05 19:06:02 -0700

    Shortened length of unique indexes.

--- ecoli/library/unique_indexes.py
+++ ecoli/library/unique_indexes.py
@@ -5,5 +5,5 @@ def create_unique_indexes(n_indexes):
 	"""
 	Creates a list of unique indexes by using uuid4() to generate each index.
 	"""
-	return [uuid4().int >> 81 for i in range(n_indexes)]
+	return [uuid4().int >> 82 for i in range(n_indexes)]
 
