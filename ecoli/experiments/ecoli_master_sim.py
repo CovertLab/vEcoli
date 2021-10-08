de1de80d68abced8204b8facaec4e21df733cbcc
matt-wolff <mbwolff@stanford.edu>
2021-10-07 19:25:13 -0700

    Created viv states and changed saving state.

--- ecoli/experiments/ecoli_master_sim.py
+++ ecoli/experiments/ecoli_master_sim.py
@@ -274,7 +274,7 @@ class EcoliSim:
             self.ecoli_experiment.update(time_to_next_save)
             state = self.ecoli_experiment.state.get_value()
             state_to_save = {key: state[key] for key in
-                             ['listeners', 'bulk', 'unique', 'environment', 'process_state']}
+                             ['listeners', 'bulk', 'unique', 'environment', 'process_state'] if key in state}
             write_json('data/vivecoli_t' + str(time_elapsed) + '.json', state_to_save)
         time_remaining = self.total_time - self.save_times[-1]
         if time_remaining:
