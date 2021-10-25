0b2d603e29f9fb055e8810edd36f987eeafa8089
cs <cs.temporary@icloud.com>
2021-10-25 13:58:04 +0000

    Parallelize Transcript Initiation and Enhance Test
    Upgrade the testing harness to include utility functions for comparing
    large states and to limit output by halting tests once an error is
    detected. Also add Transcript Initiation as a parallelized process.

--- ecoli/composites/ecoli_master_parallelization_test.py
+++ ecoli/composites/ecoli_master_parallelization_test.py
@@ -4,6 +4,7 @@ import os
 import unittest
 
 import numpy as np
+import unum
 from vivarium.core.engine import Engine
 
 from ecoli.composites.ecoli_configs import CONFIG_DIR_PATH
@@ -12,9 +13,70 @@ from ecoli.experiments.ecoli_master_sim import EcoliSim
 
 PARALLELIZED_PROCESSES = (
     'ecoli-transcript-elongation',
+    'ecoli-transcript-initiation',
 )
 TIMESTEP = 2
 TOTAL_TIME = 10
+
+
+def assert_equal(data1, data2, **kwargs):
+    if data1 == data2:
+        return
+    raise AssertionError(
+        f'Values unequal: {data1} != {data2}\n'
+        f'Metadata: {kwargs}'
+    )
+
+
+def assert_random_state_equal(rs1, rs2, **kwargs):
+    type1, keys1, pos1, has_gauss1, gauss1 = rs1.get_state()
+    type2, keys2, pos2, has_gauss2, gauss2 = rs2.get_state()
+
+    if type1 != type2:
+        raise AssertionError(
+            f'BitGenerator types unequal: {type1} != {type2}\n'
+            f'Metadata: {kwargs}'
+        )
+    np.testing.assert_array_equal(
+        keys1, keys2, f'RandomState keys unequal. Meta: {kwargs}')
+    if pos1 != pos2:
+        raise AssertionError(
+            f'pos values unequal: {pos1} != {pos2}\n'
+            f'Metadata: {kwargs}'
+        )
+    if has_gauss1 != has_gauss2:
+        raise AssertionError(
+            f'has_gauss values unequal: {has_gauss1} != {has_gauss2}\n'
+            f'Metadata: {kwargs}'
+        )
+    if gauss1 != gauss2:
+        raise AssertionError(
+            f'cached_gaussian values unequal: {gauss1} != {gauss2}\n'
+            f'Metadata: {kwargs}'
+        )
+
+
+def assert_dict_equal(data1, data2, path=tuple()):
+    assert_equal(type(data1), type(data2), path=path)
+    if isinstance(data1, np.ndarray):
+        np.testing.assert_array_equal(
+            data1, data2, f'Path: {path}')
+    elif isinstance(data1, unum.Unum):
+        assert_equal(data1.strUnit(), data2.strUnit(), path=path)
+        assert_dict_equal(data1.asNumber(), data2.asNumber(), path=path)
+    elif str(type(data1)) == "<class 'method'>":
+        assert_dict_equal(data1.__func__, data2.__func__, path=path)
+    elif str(type(data1)) == "<class 'function'>":
+        assert_equal(data1.__qualname__, data2.__qualname__, path=path)
+    elif isinstance(data1, np.random.RandomState):
+        assert_random_state_equal(data1, data2, path=path)
+    elif isinstance(data1, dict):
+        assert_equal(data1.keys(), data2.keys(), path=path)
+        for key in data1.keys():
+            assert_dict_equal(
+                data1[key], data2[key], path=path + (key,))
+    else:
+        assert_equal(data1, data2, path=path)
 
 
 class TestableEcoliSim(EcoliSim):
@@ -67,20 +129,38 @@ class TestableEcoliSim(EcoliSim):
 class EcoliParallelizationTests(unittest.TestCase):
 
     def _assertDataEqual(self, data1, data2, path=tuple(), **kwargs):
-        with self.subTest(path=path, **kwargs):
+        try:
             self.assertEqual(type(data1), type(data2))
+        except AssertionError as e:
+            kwargs['path'] = path
+            print('Metadata:', kwargs)
+            raise e
+
         if isinstance(data1, np.ndarray):
-            with self.subTest(path=path, **kwargs):
+            try:
                 np.testing.assert_array_equal(data1, data2)
+            except AssertionError as e:
+                kwargs['path'] = path
+                print('Metadata:', kwargs)
+                raise e
         elif isinstance(data1, dict):
-            with self.subTest(path=path, **kwargs):
+            try:
                 self.assertEqual(data1.keys(), data2.keys())
+            except AssertionError as e:
+                kwargs['path'] = path
+                print('Metadata:', kwargs)
+                raise e
+
             for key in data1.keys():
                 self._assertDataEqual(
                     data1[key], data2[key], path + (key,), **kwargs)
         else:
-            with self.subTest(path=path, **kwargs):
+            try:
                 self.assertEqual(data1, data2)
+            except AssertionError as e:
+                kwargs['path'] = path
+                print('Metadata:', kwargs)
+                raise e
 
     def test_parallelization(self):
         parallel_sim = TestableEcoliSim(PARALLELIZED_PROCESSES)
@@ -104,3 +184,14 @@ class EcoliParallelizationTests(unittest.TestCase):
 
         parallel_sim.end()
         non_parallel_sim.end()
+
+
+def main():
+    sim = TestableEcoliSim(PARALLELIZED_PROCESSES)
+    sim.prepare()
+
+
+if __name__ == '__main__':
+    # Added for PDB. See https://stackoverflow.com/a/60922965.
+    __spec__ = None
+    main()
