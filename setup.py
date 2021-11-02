34dc158720019834ee8e8f0db4084ed9e5378f96
cs <cs.temporary@icloud.com>
2021-11-02 16:15:04 +0000

    Require vivarium-core 0.4.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=[
-        'vivarium-core>=0.3.8',
+        'vivarium-core>=0.4.0',
         'vivarium-multibody>=0.0.15',
         'biopython==1.77',
         'Unum==4.1.4',
