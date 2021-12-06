06b5108735008a327ea50a1d8a1ea8b01f0599a9
cs <cs.temporary@icloud.com>
2021-12-06 20:18:20 +0000

    Add and add support for parallel config file

--- ecoli/composites/ecoli_master.py
+++ ecoli/composites/ecoli_master.py
@@ -117,6 +117,9 @@ class Ecoli(Composer):
                 if 'seed' in process_configs[process]:
                     process_configs[process]['seed'] = process_configs[process]['seed'] + config['seed']
 
+            if 'parallel' not in process_configs[process]:
+                process_configs[process]['_parallel'] = parallel
+
         # make the processes
         processes = {
             process_name: process(process_configs[process_name])
