9da6a81284b2b96c60b2b694324a677850a6befa
matt-wolff <mbwolff@stanford.edu>
2021-12-21 00:41:39 -0300

    Changed config in ecoli_master division test.

--- ecoli/composites/ecoli_master.py
+++ ecoli/composites/ecoli_master.py
@@ -88,7 +88,7 @@ class Ecoli(Composer):
     def initial_state(self, config=None, path=()):
         # Use initial state calculated with trna_charging and translationSupply disabled
         config = config or {}
-        initial_state_file = config.get('initial_state', 'wcecoli_t0')
+        initial_state_file = config.get('initial_state_file', 'wcecoli_t0')
         initial_state = get_state_from_file(path=f'data/{initial_state_file}.json')
         embedded_state = {}
         assoc_path(embedded_state, path, initial_state)
@@ -332,7 +332,7 @@ def test_division(
     """tests that a cell can be divided and keep running"""
 
     # get initial mass from Ecoli composer
-    initial_state = Ecoli({}).initial_state({'initial_state': 'vivecoli_t1840'})
+    initial_state = Ecoli({}).initial_state({'initial_state_file': 'vivecoli_t1840'})
 
     # make a new composer under an embedded path
     config = {
@@ -425,7 +425,7 @@ def test_division_topology():
     timestep = 2
 
     # get initial mass from Ecoli composer
-    initial_state = Ecoli({}).initial_state({'initial_state': 'vivecoli_t1840'})
+    initial_state = Ecoli({}).initial_state({'initial_state_file': 'vivecoli_t1840'})
     initial_mass = initial_state['listeners']['mass']['cell_mass']
     division_mass = initial_mass + 0.1
     print(f"DIVIDE AT {division_mass} fg")
