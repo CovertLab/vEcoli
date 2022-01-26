5fc590b84a2b6752153eb3a28b65cfb97085b3bb
cs <cs.temporary@icloud.com>
2022-01-26 01:44:35 +0000

    Include values with units in multigen plot

--- ecoli/analysis/analyze_multigen_db_experiment.py
+++ ecoli/analysis/analyze_multigen_db_experiment.py
@@ -6,12 +6,23 @@ from vivarium.core.emitter import (
     data_from_database,
     DatabaseEmitter,
 )
+from vivarium.core.serialize import deserialize_value
 from vivarium.plots.agents_multigen import plot_agents_multigen
+from vivarium.library.topology import get_in, assoc_path
+from vivarium.library.units import remove_units
 
 from ecoli.composites.ecoli_nonpartition import SIM_DATA_PATH
 from ecoli.analysis.analyze_db_experiment import access, OUT_DIR
 
 OUT_DIR = 'out/analysis/'
+SERIALIZED_PATHS = (
+    ('periplasm', 'global', 'mmol_to_counts'),
+    ('periplasm', 'global', 'volume'),
+    ('boundary', 'surface_area'),
+    ('boundary', 'mmol_to_counts'),
+    ('boundary', 'mass'),
+)
+AGENTS_PATH = ('agents',)
 
 
 def main():
@@ -25,6 +36,15 @@ def main():
 
     # Retrieve all simulation data.
     data, experiment_id, sim_config = access(args.experiment_id)
+    for time, time_data in data.items():
+        for agent, agent_data in get_in(time_data, AGENTS_PATH).items():
+            for path_suffix in SERIALIZED_PATHS:
+                path = (time,) + AGENTS_PATH + (agent,) + path_suffix
+                serialized = get_in(data, path)
+                assert serialized is not None
+                deserialized = deserialize_value(serialized)
+                assoc_path(data, path, deserialized)
+    data = remove_units(data)
 
     plot_agents_multigen(
         data,
