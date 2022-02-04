371647dfb477bbaaf2296cb62c10c5152b477bcf
matt-wolff <mbwolff@stanford.edu>
2022-02-04 12:39:09 -0800

    Added timeline process to porin_fickian

--- ecoli/composites/porin_fickian.py
+++ ecoli/composites/porin_fickian.py
@@ -6,12 +6,14 @@ from vivarium.plots.simulation_output import plot_variables
 from ecoli.processes.antibiotics.porin_permeability import PorinPermeability, CEPH_OMPC_CON_PERM,\
     CEPH_OMPF_CON_PERM, SA_AVERAGE
 from ecoli.processes.antibiotics.fickian_diffusion import FickianDiffusion
+from vivarium.processes.timeline import TimelineProcess
 from ecoli.states.wcecoli_state import get_state_from_file
 
 
 class PorinFickian(Composer):
     defaults = {
         'fickian': {},
+        'timeline': {},
         'porin_permeability': {},
     }
 
@@ -20,7 +22,9 @@ class PorinFickian(Composer):
 
     def generate_processes(self, config):
         fick_diffusion = FickianDiffusion(config['fickian'])
-        return {'fickian': fick_diffusion}
+        timeline = TimelineProcess(config['timeline'])
+        return {'fickian': fick_diffusion,
+                'timeline': timeline}
 
     def generate_topology(self, config):
         return {
@@ -32,6 +36,10 @@ class PorinFickian(Composer):
                 'volume_global': ('listeners', 'mass',),
                 'mass_global': ('listeners', 'mass',),
                 'permeabilities': ('boundary', 'permeabilities')
+            },
+            'timeline': {
+                'global': ('global',),  # The global time is read here
+                'porins': ('bulk',),  # This port is based on the declared timeline
             },
             'porin_permeability': {
                 'porins': ('bulk',),
@@ -47,7 +55,26 @@ class PorinFickian(Composer):
 
 def main():
     sim_time = 10
+
+    initial_state = get_state_from_file(path='data/vivecoli_t1000.json')
+    initial_state['boundary'] = {}
+    initial_state['boundary']['surface_area'] = SA_AVERAGE
+    initial_state['listeners']['mass']['dry_mass'] = initial_state['listeners']['mass']['dry_mass'] * units.fg
+
+    timeline = []
+    for i in range(10):
+        timeline.append(
+            (i, {
+                ('porins', 'CPLX0-7533[o]'): initial_state['bulk']['CPLX0-7533[o]'] + ((i + 1) * 500),
+                ('porins', 'CPLX0-7534[o]'): initial_state['bulk']['CPLX0-7534[o]'] + ((i + 1) * 500),
+            })
+        )
+
     config = {
+        'timeline': {
+            'time_step': 1.0,
+            'timeline': timeline,
+        },
         'porin_permeability': {
             'porin_ids': ['CPLX0-7533[o]', 'CPLX0-7534[o]'],
             'diffusing_molecules': {
@@ -71,11 +98,6 @@ def main():
     }
     composer = PorinFickian(config)
     composite = composer.generate()
-
-    initial_state = get_state_from_file(path='data/vivecoli_t1000.json')
-    initial_state['boundary'] = {}
-    initial_state['boundary']['surface_area'] = SA_AVERAGE
-    initial_state['listeners']['mass']['dry_mass'] = initial_state['listeners']['mass']['dry_mass'] * units.fg
 
     sim = Engine(composite=composite, initial_state=initial_state)
     sim.update(sim_time)
