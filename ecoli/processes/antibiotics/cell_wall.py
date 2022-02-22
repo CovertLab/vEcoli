2813aa5ba66ee375c3c5396ed3c7a0bc4e11edd1
robotato <mica@mica-gram.localdomain>
2022-02-22 14:27:17 -0800

    prototype

--- ecoli/processes/antibiotics/cell_wall.py
+++ ecoli/processes/antibiotics/cell_wall.py
@@ -1,17 +1,28 @@
-from matplotlib import pyplot as plt
-import numpy as np
-from vivarium.core.composition import simulate_process
+"""
+TODO: references for parameters
+"""
 
+import os
+from random import choice
+import numpy as np
+from matplotlib import pyplot as plt
+
+from vivarium.library.units import units
+
+from vivarium.core.engine import Engine
+from vivarium.core.composition import simulate_process, simulate_composite
 from vivarium.core.process import Process
 from vivarium.plots.simulation_output import plot_variables
 
 from ecoli.processes.registries import topology_registry
+from ecoli.library.schema import bulk_schema
 
 
 # Register default topology for this process, associating it with process name
-NAME = 'ecoli-tf-binding'
+NAME = 'ecoli-cell-wall'
 TOPOLOGY = {
-        
+    'shape': ('shape',),
+    'bulk_murein': ("bulk",),
 }
 topology_registry.register(NAME, TOPOLOGY)
 
@@ -21,67 +32,244 @@ class CellWall(Process):
     name = NAME
     topology = TOPOLOGY
     defaults = {
-        'cracked' : False,
-        'murein' : 'CPD-12261[p]',
-        'beta_lactam' : "", # TBD
-        'SLT' : '', # TBD
-        'PBP1A' : 'CPLX0-7717[i]', # transglycosylase-transpeptidase ~100
-        'PBP1B' : 'CPLX0-3951[i]', # transglycosylase-transpeptidase ~100
-        'PBP1C' : 'G7322-MONOMER[i]', # transglycosylase
-        'PBP2' : 'EG10606-MONOMER[i]', # transpeptidase ~20
-        'PBP3' : 'EG10341-MONOMER[i]', # transglycosylase-transpeptidase ~50
-        'PBP4' : 'EG10202-MONOMER[p]', # DD-endopeptidase, DD-carboxypeptidase ~110
-        'PBP5' : 'EG10201-MONOMER[i]', # DD-caroxypeptidase ~1,800
-        'PBP6' : 'EG10203-MONOMER[i]', # DD-carbocypeptidase ~600
-        'PBP7' : 'EG12015-MONOMER[p]', # DD-endopeptidase
-        'SURFACE_AREA_PER_MOLECULE' : {		# in um^2 / molecule
-            'LPS': 1.42E-6,
-            'porins_and_ompA': 9E-6,
-            'phospholipids': 4.71E-07,
-            'lipoprotein': 7.14E-07,
-        }
+        # Molecules
+        'murein': 'CPD-12261[p]',  # two crosslinked peptidoglycan units
+        'SLT': '',  # TBD
+        'PBP': {  # penicillin-binding proteins
+            'PBP1A': 'CPLX0-7717[i]',  # transglycosylase-transpeptidase ~100
+            'PBP1B': 'CPLX0-3951[i]'  # transglycosylase-transpeptidase ~100
+        },
+        'cephaloridine': '',
+
+        # Physical parameters
+        'critical_radius': 20 * units.nm,
+        'cell_radius': 1 * units.um,
+        # 4.1 in maximally stretched configuration,
+        'disaccharide_length': 1.03 * units.nm,
+        # divided by 3 because the sacculus can be stretched threefold
+        'crossbridge_length': 	4.1 * units.nm / 3,
+
+        # Simulation parameters
+        'seed': 0
     }
 
     def __init__(self, parameters=None):
         super().__init__(parameters)
 
+        # Get murein id and keep track of murein from last timestep
+        self.murein = self.parameters['murein']
+        self.murein_prev = 0
+
+        self.critical_radius = 20 * units.nm
+        self.critical_area = np.pi * self.critical_radius**2
+
+        # Create pseudorandom number generator
+        self.rng = np.random.default_rng(self.parameters['seed'])
 
     def ports_schema(self):
         schema = {
+            'bulk_murein': bulk_schema([self.parameters['murein']]),
+            'PBP': bulk_schema(self.parameters['PBP'].values()),
 
+            'shape': {
+                "length": {
+                    '_default': 2 * units.um  # TODO: set to 0
+                }
+            },
+
+            'wall_state': {
+                'lattice': {
+                    '_default': list()  # A sparse, set-of-defects representation of the cell wall
+                },
+                'lattice_rows': {
+                    '_default': 0
+                },
+                'lattice_cols': {
+                    '_default': 0
+                },
+                'cracked': {
+                    '_default': False
+                }
+            }
         }
 
         return schema
 
-
     def next_update(self, timestep, states):
-        # Cell wall construction/destruction, beta-lactam activity
+        DEBUG = True
 
-        # Crack detection
-        
-        # Cracking is irreversible
-        cracked = self.cracked or False
+        # Unpack states
+        length = states['shape']['length']
+        lattice = states['wall_state']['lattice']
+        lattice_rows = states['wall_state']['lattice_rows']
+        lattice_cols = states['wall_state']['lattice_cols']
 
-        update = {
-            "cracked" : cracked
+        update = {}
+
+        if DEBUG:
+            states['bulk_murein'][self.murein] = 3000000
+            update['shape'] = {"length": length + 0.1 * units.um}
+            pass
+
+        # Expand lattice size if necessary, depending on cell size
+        print("resizing lattice")
+        lattice, rows, columns = self.resize_lattice(length, self.parameters['cell_radius'],
+                                                     lattice, lattice_rows, lattice_cols)
+
+        # Cell wall construction/destruction
+        print("assigning murein")
+        d_murein = 2 * (states['bulk_murein'][self.murein] - self.murein_prev)  # One murein unit represents 2 PG subunits
+        self.murein_prev = states['bulk_murein'][self.murein]
+        lattice = self.assign_murein(d_murein, lattice, rows, columns)
+
+        update['wall_state'] = {
+            'lattice': lattice,
+            'lattice_rows': rows,
+            'lattice_cols': columns
         }
+
+        # Cephaloridine activity
+        # TODO
+
+        # Crack detection (cracking is irreversible)
+        print("crack detection")
+        if (not states['wall_state']['cracked']
+                and self.get_largest_defect_area(lattice) > self.critical_area):
+            update['wall_state']['cracked'] = True
+
         return update
 
 
+    def resize_lattice(self, cell_length, cell_radius,
+                       lattice, prev_rows=0, prev_cols=0):
+
+        # Calculate new lattice size
+        columns = int(cell_length / self.parameters['crossbridge_length'])
+        self.circumference = 2 * np.pi * cell_radius
+        rows = int(self.circumference / self.parameters['disaccharide_length'])
+
+        # Fill in new positions with defects initially
+        if columns > prev_cols:
+            for c in range(prev_cols, columns):
+                for r in range(rows):
+                    lattice.append((r, c))
+
+        if rows > prev_rows:
+            for r in range(prev_rows, rows):
+                for c in range(columns):
+                    lattice.append((r, c))
+
+        return lattice, rows, columns
+
+
+    def assign_murein(self, d_murein, lattice, rows, columns):
+        adding_murein = d_murein > 0
+
+        if adding_murein:  # fill holes
+            defects = len(lattice)
+            fill_n = min(d_murein, defects)
+            
+            fill_idx = self.rng.choice(np.arange(len(lattice)), size=fill_n, replace=False)
+            lattice = [lattice[i] for i in range(len(lattice)) if i not in fill_idx]
+        else:  # add holes
+            for _ in range(d_murein):
+                candidate = (self.rng.integers(rows), self.rng.integers(columns))
+                while candidate in lattice:
+                    candidate = (self.rng.integers(rows), self.rng.integers(columns))
+                lattice.add(candidate)
+        
+        return lattice
+
+
+
+
+
+
+    def get_largest_defect_area(self, lattice):
+        # TODO: generate hole-view from defects
+        max_size = 0
+
+        # TODO: replace with actual area from literature
+        return max_size * 4 * units.nm**2
+
+
+    def get_full_lattice(self, sparse_lattice, rows, cols):
+        result = np.ones((rows, cols))
+
+        for r, c in sparse_lattice:
+            result[r, c] = 0
+
+        return result
+
+
+def plot_lattice(lattice):
+    fig, axs = plt.subplot()
+    fig.imshow(lattice, interpolation='nearest')
+    return fig, axs
+
+
 def main():
+    from vivarium.processes.timeline import TimelineProcess
+    from vivarium.core.composer import Composite
+
+    # Stub for rest of cell (increasing murein)
+    cell_stub = TimelineProcess({
+        'time_step': 2.0,
+        'timeline': [(time, {('bulk_murein', 'CPD-12261[p]'): int(3e6 + 1000 * time)}) for time in range(0, 10, 2)]
+    })
+
+    # Cell wall process
     params = {
 
     }
+    cell_wall = CellWall(params)
 
-    cellwall = CellWall(params)
-    data = simulate_process(cellwall, {'total_time': 10})
-    # fig = plot_variables(
-    #     data,
-    #     variables=[
-    #         # ('internal', 'antibiotic'),
-    #         # ('internal', 'antibiotic_hydrolyzed'),
-    #     ],
-    # )
+    settings = {
+        'total_time': 10,
+        'initial_state': {
+
+        }
+    }
+
+    # sim = Engine(
+    #     processes={
+    #         # 'cell_stub': cell_stub,
+    #         "cell_wall": cell_wall
+    #     },
+    #     topology={
+    #         'cell_wall': {
+    #             'bulk_murein': ("bulk", 'CPD-12261[p]'),
+    #             'shape' : ("shape",)
+    #         },
+    #         # 'cell_stub': {
+    #         #     'global': ('global',),
+    #         #     'bulk_murein': ("bulk", 'CPD-12261[p]'),
+    #         # }
+    #     },
+    #     initial_state={
+    #         'bulk': {
+    #             'CPD-12261[p]': int(3e6),
+    #         },
+    #         'shape' : {
+    #             'length' : 2 * units.um,
+    #         }})
+    #
+    # sim.run_for(10)
+    # data = sim.emitter.get_data()
+
+    data = simulate_process(cell_wall, settings)
+    #data = simulate_process(cell_stub, settings)
+    #data = simulate_composite(test_composite, settings)
+    fig = plot_variables(
+        data,
+        variables=[
+            ("bulk_murein", 'CPD-12261[p]'),
+        ],
+    )
+    fig.tight_layout()
+
+    os.makedirs("out/processes/cell_wall/", exist_ok=True)
+    fig.savefig("out/processes/cell_wall/test.png")
 
 
 if __name__ == '__main__':
