41ea1e389a9edc7ae12872b8816f9ab3206971ef
matt-wolff <mbwolff@stanford.edu>
2022-03-15 15:28:29 -0700

    Restored files.

--- ecoli/processes/antibiotics/fickian_diffusion.py
+++ ecoli/processes/antibiotics/fickian_diffusion.py
@@ -19,29 +19,24 @@ AVOGADRO = constants.N_A * 1 / units.mol
 class FickianDiffusion(Process):
 
     name = "fickian_diffusion"
-    # defaults = {
-    #     'molecules_to_diffuse': ['antibiotic'],
-    #     'initial_state': {
-    #         'internal': {
-    #             'antibiotic': 0,  # mM
-    #         },
-    #         'external': {
-    #             'antibiotic': 1e-3,  # mM
-    #         },
-    #         'mass_global': {
-    #             'dry_mass': 300 * units.fg,
-    #         },
-    #         'volume_global': {
-    #             'volume': 1.2 * units.fL,
-    #         },
-    #     },
-    #     'default_default': 0,
-    #     'surface_area_mass_ratio': 132 * units.cm**2 / units.mg,
-    # }
-
-    # old defaults were merging w/ new config instead of being replaced by new config?
     defaults = {
-        'default_default': 0
+        'molecules_to_diffuse': ['antibiotic'],
+        'initial_state': {
+            'internal': {
+                'antibiotic': 0,  # mM
+            },
+            'external': {
+                'antibiotic': 1e-3,  # mM
+            },
+            'mass_global': {
+                'dry_mass': 300 * units.fg,
+            },
+            'volume_global': {
+                'volume': 1.2 * units.fL,
+            },
+        },
+        'default_default': 0,
+        'surface_area_mass_ratio': 132 * units.cm**2 / units.mg,
     }
 
     def ports_schema(self):
