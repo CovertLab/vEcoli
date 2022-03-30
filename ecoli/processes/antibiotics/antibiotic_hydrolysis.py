Fix tests for added units

ecoli/processes/antibiotics/antibiotic_hydrolysis.py
@@ -111,8 +111,8 @@ def demo():
     fig = plot_variables(
         data,
         variables=[
-            ('internal', 'antibiotic'),
-            ('internal', 'antibiotic_hydrolyzed'),
+            ('internal', ('antibiotic', 'millimolar')),
+            ('internal', ('antibiotic_hydrolyzed', 'millimolar')),
         ],
     )
     return fig, data
@@ -151,13 +151,14 @@ def test_antibiotic_hydrolysis():
     expected_data = get_expected_demo_data()
     assert simulated_data['time'] == expected_data['time']
     np.testing.assert_allclose(
-        simulated_data['internal']['antibiotic'],
+        simulated_data['internal'][('antibiotic', 'millimolar')],
         expected_data['antibiotic'],
         rtol=0,
         atol=1e-15,
     )
     np.testing.assert_allclose(
-        simulated_data['internal']['antibiotic_hydrolyzed'],
+        simulated_data['internal'][
+            ('antibiotic_hydrolyzed', 'millimolar')],
         expected_data['antibiotic_hydrolyzed'],
         rtol=0,
         atol=1e-15,
@@ -167,7 +168,8 @@ def test_antibiotic_hydrolysis():
 def get_demo_vs_expected_plot(demo_data, expected_data):
     fig, (ax1, ax2) = plt.subplots(nrows=2, figsize=(10, 5))
     ax1.plot(
-        demo_data['time'], demo_data['internal']['antibiotic'],
+        demo_data['time'], demo_data['internal'][
+            ('antibiotic', 'millimolar')],
         label='simulated', alpha=0.5,
     )
     ax1.plot(
@@ -181,7 +183,8 @@ def get_demo_vs_expected_plot(demo_data, expected_data):
 
     ax2.plot(
         demo_data['time'],
-        demo_data['internal']['antibiotic_hydrolyzed'],
+        demo_data['internal'][
+            ('antibiotic_hydrolyzed', 'millimolar')],
         label='simulated', alpha=0.5,
     )
     ax2.plot(
@@ -195,3 +198,7 @@ def get_demo_vs_expected_plot(demo_data, expected_data):
     fig.tight_layout()
 
     return fig
+
+
+if __name__ == '__main__':
+    test_antibiotic_hydrolysis()

ecoli/processes/environment/local_field.py
@@ -110,20 +110,20 @@ def test_local_fields():
     parameters = {}
     local_fields_process = LocalField(parameters)
 
-    bounds = [5, 5]
+    bounds = [5, 5] * units.um
     n_bins = [3, 3]
     initial_state = {
         'exchanges': {
             'A': 20
         },
-        'location': [0.5, 0.5],
+        'location': [0.5, 0.5] * units.um,
         'fields': {
             'A': np.ones((n_bins[0], n_bins[1]), dtype=np.float64)
         },
         'dimensions': {
             'bounds': bounds,
             'n_bins': n_bins,
-            'depth': 1,
+            'depth': 1 * units.um,
         }
     }
 