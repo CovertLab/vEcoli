e1c41226a76a0da06cac10b522691f6b0af339e5
matt-wolff <mbwolff@stanford.edu>
2022-03-31 20:35:38 -0700

    Added tetracycline to ext_periplasm sbml script.

--- scripts/generate_ext_periplasm_sbml.py
+++ scripts/generate_ext_periplasm_sbml.py
@@ -27,6 +27,7 @@ from vivarium.library.units import units
 
 DATA_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'data'))
+
 # Calculated by dividing V_max reported in (Nagano & Nikaido, 2009) by the model's initial pump concentration of
 # 20.179269875115253 counts / micron^2
 CEPH_PUMP_KCAT = 0.0956090147363198  # / units.sec
@@ -36,6 +37,27 @@ CEPH_PUMP_KM = 288e-3  # * units.millimolar
 CEPH_BETA_LACTAMASE_KCAT = 130  # / units.sec
 # Reported in (Galleni et al., 1988)
 CEPH_BETA_LACTAMASE_KM = 170  # * units.micromolar
+# Cephaloridine default permeability = ompF permeability + ompC permeability (Nikaido, 1983)
+DEFAULT_CEPH_OUTER_PERM = 52.6e-5 + 4.5e-5  # cm/sec
+
+# Calculated by dividing V_max estimated in (Thanassi et al., 1995) by the model's initial pump concentration of
+# 20.179269875115253 counts / micron^2
+TET_PUMP_KCAT = 0.00015759727703788977  # / units.sec
+# Estimated in (Thanassi et al., 1995)
+TET_PUMP_KM = 200e-3  # * units.millimolar
+# Estimated in (Thanassi et al., 1995)
+DEFAULT_TET_OUTER_PERM = 1e-5  # cm/sec
+
+AREA_MASS_RATIO = 132  # cm^2/mg
+MASS = 1170e-12  # mg
+VOLUME = 0.32e-12  # mL
+INITIAL_PERIPLASM_CEPH = 0
+INITIAL_ENVIRONMENT_CEPH = 0.1239
+INITIAL_HYDROLYZED_CEPH = 0
+INITIAL_PERIPLASM_TET = 0
+INITIAL_ENVIRONMENT_TET = 0.1239
+INITIAL_PUMP = 0.0004525
+INITIAL_BETA_LACTAMASE = 0.000525
 
 
 # A ProportionalHillPositive propensity generates a propensity:
@@ -61,6 +83,8 @@ def main() -> None:
     cephaloridine_in = Species('cephaloridine_periplasm')
     cephaloridine_out = Species('cephaloridine_environment')
     cephaloridine_hydrolyzed = Species('cephaloridine_hydrolyzed')
+    tetracycline_in = Species('tetracycline_periplasm')
+    tetracycline_out = Species('tetracycline_environment')
 
     pump = Species('AcrAB')
     beta_lactamase = Species('beta_lactamase')
@@ -68,23 +92,38 @@ def main() -> None:
     species = [
         cephaloridine_in,
         cephaloridine_out,
+        tetracycline_in,
+        tetracycline_out,
         cephaloridine_hydrolyzed,
         pump,
         beta_lactamase,
     ]
 
     # Define reactions.
-    export_propensity = ProportionalHillPositive(
+    ceph_export_propensity = ProportionalHillPositive(
         k=ParameterEntry('export_kcat', CEPH_PUMP_KCAT),  # Hz
         s1=cephaloridine_in,
         K=ParameterEntry('export_km', CEPH_PUMP_KM),  # mM
         d=pump,
         n=1.75,
     )
-    export = Reaction(
+    ceph_export = Reaction(
         inputs=[cephaloridine_in],
         outputs=[cephaloridine_out],
-        propensity_type=export_propensity,
+        propensity_type=ceph_export_propensity,
+    )
+
+    tet_export_propensity = ProportionalHillPositive(
+        k=ParameterEntry('export_kcat', TET_PUMP_KCAT),  #Hz
+        s1=tetracycline_in,
+        K=ParameterEntry('export_km', TET_PUMP_KM),  #mM
+        d=pump,
+        n=1
+    )
+    tet_export = Reaction(
+        inputs=[tetracycline_in],
+        outputs=[tetracycline_out],
+        propensity_type=tet_export_propensity
     )
 
     hydrolysis_propensity = ProportionalHillPositive(
@@ -100,40 +139,57 @@ def main() -> None:
         propensity_type=hydrolysis_propensity,
     )
 
-    area_mass_ratio = ParameterEntry('x_am', 132)  # cm^2/mg
-    permeability = ParameterEntry('perm', 52.6e-5 + 4.5e-5)  # cm/sec, ompF permeability + ompC permeability (Nikaido, 1983)
-    mass = ParameterEntry('mass', 1170e-12)  # mg
-    volume = ParameterEntry('volume', 0.32e-12)  # mL
-    influx_propensity = GeneralPropensity(
+    area_mass_ratio = ParameterEntry('x_am', AREA_MASS_RATIO)  # cm^2/mg
+    ceph_permeability = ParameterEntry('perm', DEFAULT_CEPH_OUTER_PERM)  # cm/sec
+    tet_permeability = ParameterEntry('perm', DEFAULT_TET_OUTER_PERM)  # cm/sec
+    mass = ParameterEntry('mass', MASS)  # mg
+    volume = ParameterEntry('volume', VOLUME)  # mL
+    ceph_influx_propensity = GeneralPropensity(
         (
             f'x_am * perm * ({cephaloridine_out} - {cephaloridine_in}) '
             '* mass / (volume)'
         ),
         propensity_species=[cephaloridine_in, cephaloridine_out],
         propensity_parameters=[
-            area_mass_ratio, permeability, mass, volume],
+            area_mass_ratio, ceph_permeability, mass, volume],
     )
-    influx = Reaction(
+    ceph_influx = Reaction(
         inputs=[cephaloridine_out],
         outputs=[cephaloridine_in],
-        propensity_type=influx_propensity
+        propensity_type=ceph_influx_propensity
+    )
+    tet_influx_propensity = GeneralPropensity(
+        (
+            f'x_am * perm * ({tetracycline_out} - {tetracycline_in}) '
+            '* mass / (volume)'
+        ),
+        propensity_species=[tetracycline_in, tetracycline_out],
+        propensity_parameters=[
+            area_mass_ratio, tet_permeability, mass, volume],
+    )
+    tet_influx = Reaction(
+        inputs=[tetracycline_out],
+        outputs=[tetracycline_in],
+        propensity_type=tet_influx_propensity
     )
 
     initial_concentrations = {
-        cephaloridine_in: 0,
-        cephaloridine_out: 0.1239,
-        cephaloridine_hydrolyzed: 0,
-        pump: 0.0004525,
-        beta_lactamase: 0.000525,
+        cephaloridine_in: INITIAL_PERIPLASM_CEPH,
+        cephaloridine_out: INITIAL_ENVIRONMENT_CEPH,
+        cephaloridine_hydrolyzed: INITIAL_HYDROLYZED_CEPH,
+        pump: INITIAL_PUMP,
+        tetracycline_in: INITIAL_PERIPLASM_TET,
+        tetracycline_out: INITIAL_ENVIRONMENT_TET,
+        beta_lactamase: INITIAL_BETA_LACTAMASE,
     }
 
     crn = ChemicalReactionNetwork(
         species=species,
-        reactions=[export, hydrolysis, influx],
+        reactions=[ceph_export, tet_export, hydrolysis, ceph_influx, tet_influx],
         initial_concentration_dict=initial_concentrations,
     )
 
-    path = os.path.join(DATA_DIR, 'bioscrape_sbml.xml')
+    path = os.path.join(DATA_DIR, 'ext_periplasm_sbml.xml')
     print(f'Writing the following CRN to {path}:')
     print(crn.pretty_print(show_rates=True))
     crn.write_sbml_file(path)
