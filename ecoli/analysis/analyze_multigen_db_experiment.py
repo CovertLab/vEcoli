3708e88f0fcca6e0e0c04cce0d93e50ca5370119
cs <cs.temporary@icloud.com>
2022-04-11 16:54:47 +0000

    Deserialize all data for multigen analysis

--- ecoli/analysis/analyze_multigen_db_experiment.py
+++ ecoli/analysis/analyze_multigen_db_experiment.py
@@ -7,14 +7,6 @@ from vivarium.library.units import remove_units
 
 from ecoli.analysis.analyze_db_experiment import access, OUT_DIR
 
-SERIALIZED_PATHS = (
-    ('periplasm', 'global', 'mmol_to_counts'),
-    ('periplasm', 'global', 'volume'),
-    ('boundary', 'surface_area'),
-    ('boundary', 'mmol_to_counts'),
-    ('boundary', 'mass'),
-    ('permeabilities',),
-)
 AGENTS_PATH = ('agents',)
 
 
@@ -35,14 +27,6 @@ def main():
     data, experiment_id, sim_config = access(
         args.experiment_id, host=args.host, port=args.port)
     data = deserialize_value(data)
-    #for time, time_data in data.items():
-    #    for agent, agent_data in get_in(time_data, AGENTS_PATH).items():
-    #        for path_suffix in SERIALIZED_PATHS:
-    #            path = (time,) + AGENTS_PATH + (agent,) + path_suffix
-    #            serialized = get_in(data, path)
-    #            assert serialized is not None
-    #            deserialized = deserialize_value(serialized)
-    #            assoc_path(data, path, deserialized)
     data = remove_units(data)
 
     plot_agents_multigen(
