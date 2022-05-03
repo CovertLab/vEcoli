969b938c0ba7086d5f64a4f676c48fa035e58476
matt-wolff <mbwolff@stanford.edu>
2022-05-02 22:35:41 -0700

    Implemented simple colony saving and loading.

--- ecoli/states/wcecoli_state.py
+++ ecoli/states/wcecoli_state.py
@@ -20,9 +20,10 @@ def load_states(path):
     with open(path, 'r') as states_file:
         states = json.load(states_file)
 
-    states['environment'] = {
-        key: infinitize(value)
-        for key, value in states['environment'].items()}
+    if 'environment' in states:
+        states['environment'] = {
+            key: infinitize(value)
+            for key, value in states['environment'].items()}
 
     return states
 
@@ -34,55 +35,61 @@ def get_state_from_file(
 
     states = load_states(path)
 
-    initial_state = {
-        'environment': {
-            'media_id': 'minimal',
-            # TODO(Ryan): pull in environmental amino acid levels
-            'amino_acids': {},
-            'exchange_data': {
-                'unconstrained': {
-                    'CL-[p]',
-                    'FE+2[p]',
-                    'CO+2[p]',
-                    'MG+2[p]',
-                    'NA+[p]',
-                    'CARBON-DIOXIDE[p]',
-                    'OXYGEN-MOLECULE[p]',
-                    'MN+2[p]',
-                    'L-SELENOCYSTEINE[c]',
-                    'K+[p]',
-                    'SULFATE[p]',
-                    'ZN+2[p]',
-                    'CA+2[p]',
-                    'Pi[p]',
-                    'NI+2[p]',
-                    'WATER[p]',
-                    'AMMONIUM[c]'},
-                'constrained': {
-                    'GLC[p]': 20.0 * units.mmol / (units.g * units.h)}},
-            'external_concentrations': states['environment']},
-        # TODO(Eran): deal with mass
-        # add mw property to bulk and unique molecules
-        # and include any "submass" attributes from unique molecules
-        'listeners': states['listeners'],
-        'bulk': states['bulk'],
-        'unique': {},
-        'process_state': {
-            'polypeptide_elongation': {}}}
-    
-    for mol_type, molecules in states['unique'].items():
-        initial_state['unique'].update({mol_type: {}})
-        for molecule_id, values in molecules.items():
-            if convert_unique_id_to_string:
-                molecule_id = str(molecule_id)
-            initial_state['unique'][mol_type][molecule_id] = {'submass': np.zeros(len(MASSDIFFS))}
-            for key, value in values.items():
-                if key in MASSDIFFS:
-                    initial_state['unique'][mol_type][molecule_id]['submass'][MASSDIFFS[key]] = value
-                elif key in ['unique_index', 'RNAP_index', 'mRNA_index'] and convert_unique_id_to_string:
-                    # convert these values to strings
-                    initial_state['unique'][mol_type][molecule_id][key] = str(value)
-                else:
-                    initial_state['unique'][mol_type][molecule_id][key] = value
+    initial_state = {}
+    if 'environment' in states:
+        initial_state = {
+            'environment': {
+                'media_id': 'minimal',
+                # TODO(Ryan): pull in environmental amino acid levels
+                'amino_acids': {},
+                'exchange_data': {
+                    'unconstrained': {
+                        'CL-[p]',
+                        'FE+2[p]',
+                        'CO+2[p]',
+                        'MG+2[p]',
+                        'NA+[p]',
+                        'CARBON-DIOXIDE[p]',
+                        'OXYGEN-MOLECULE[p]',
+                        'MN+2[p]',
+                        'L-SELENOCYSTEINE[c]',
+                        'K+[p]',
+                        'SULFATE[p]',
+                        'ZN+2[p]',
+                        'CA+2[p]',
+                        'Pi[p]',
+                        'NI+2[p]',
+                        'WATER[p]',
+                        'AMMONIUM[c]'},
+                    'constrained': {
+                        'GLC[p]': 20.0 * units.mmol / (units.g * units.h)}},
+                'external_concentrations': states['environment']},
+            # TODO(Eran): deal with mass
+            # add mw property to bulk and unique molecules
+            # and include any "submass" attributes from unique molecules
+            'listeners': states['listeners'],
+            'bulk': states['bulk'],
+            'unique': {},
+            'process_state': {
+                'polypeptide_elongation': {}}}
+
+    if 'unique' in states:
+        for mol_type, molecules in states['unique'].items():
+            initial_state['unique'].update({mol_type: {}})
+            for molecule_id, values in molecules.items():
+                if convert_unique_id_to_string:
+                    molecule_id = str(molecule_id)
+                initial_state['unique'][mol_type][molecule_id] = {'submass': np.zeros(len(MASSDIFFS))}
+                for key, value in values.items():
+                    if key in MASSDIFFS:
+                        initial_state['unique'][mol_type][molecule_id]['submass'][MASSDIFFS[key]] = value
+                    elif key in ['unique_index', 'RNAP_index', 'mRNA_index'] and convert_unique_id_to_string:
+                        # convert these values to strings
+                        initial_state['unique'][mol_type][molecule_id][key] = str(value)
+                    else:
+                        initial_state['unique'][mol_type][molecule_id][key] = value
+
+    if not len(initial_state):
+        initial_state = states
 
     return initial_state
