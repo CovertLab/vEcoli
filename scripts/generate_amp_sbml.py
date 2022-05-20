16a9b22a7250da0c318325e97db5ccd5315ae1bb
cs <cs.temporary@icloud.com>
2022-05-20 17:11:30 +0000

    Move AMP parameters to separate file

--- scripts/generate_amp_sbml.py
+++ scripts/generate_amp_sbml.py
@@ -25,6 +25,7 @@ from biocrnpyler import (
 from scipy import constants
 from vivarium.library.units import units
 from vivarium.library.topology import get_in
+from ecoli.parameters import param_store
 
 DATA_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'data'))
@@ -34,109 +35,6 @@ DIFFUSION_ONLY_FILENAME = 'amp_diffusion_only_sbml.xml'
 
 MichaelisMentenKinetics = namedtuple(
     'MichaelisMentenKinetics', ('kcat', 'km', 'n'))
-
-
-class Parameter:
-
-    def __init__(self, value, source, canonicalize=None, note=''):
-        self.value = value
-        self.source = source
-        self.canonicalize = canonicalize or (lambda x: x)
-        self.note = note
-
-
-class ParameterStore:
-
-    def __init__(self, parameters):
-        self.parameters = parameters
-
-    def get(self, path):
-        param_obj = get_in(self.parameters, path)
-        if not param_obj:
-            raise RuntimeError(
-                f'No parameter found at path {path}')
-        return param_obj.canonicalize(param_obj.value)
-
-
-PARAMS = {
-    'ampicillin': {
-        'permeability': {
-            'outer': Parameter(
-                0.28e-5 * units.cm / units.sec,
-                'Kojima and Nikaido (2013)',
-                note='This is total, not per-porin, permeability.',
-            ),
-        },
-        'mic': Parameter(
-            2 * units.micrograms / units.mL,
-            'Mazzariol, Cornaglia, and Nikaido (2000)',
-            lambda x: (
-                x / (349.4 * units.g / units.mol)
-            ).to(units.mM),
-        ),
-        'efflux': {
-            'kcat': Parameter(
-                0.085 * units.nmol / units.mg / units.sec,  # Vmax
-                'Kojima and Nikaido (2013)',
-                lambda x: (
-                    x / (6.7e-4 * units.mM)  # Pump concentration
-                    * 1170 * units.fg  # Total cell mass
-                    / (0.212799 * units.fL)  # Periplasm volume
-                )
-            ),
-            'km': Parameter(
-                2.16e-3 * units.mM,
-                'Kojima and Nikaido (2013)',
-            ),
-            'n': Parameter(
-                1.9,
-                'Kojima and Nikaido (2013)',
-            ),
-        },
-        'hydrolysis': {
-            'kcat': Parameter(
-                6.5e-3 / units.sec,
-                'Mazzariol, Cornaglia, and Nikaido (2000)',
-            ),
-            'km': Parameter(
-                0.9e-3 * units.mM,
-                'Mazzariol, Cornaglia, and Nikaido (2000)',
-            ),
-            'n': Parameter(
-                1,
-                'Mazzariol, Cornaglia, and Nikaido (2000)',
-            ),
-        },
-    },
-    'shape': {
-        'periplasm_fraction': Parameter(
-            0.2,
-            'Stock et al. (1977)'
-        ),
-        'initial_cell_mass': Parameter(
-            1170 * units.fg,
-            'Model',
-        ),
-        'initial_cell_volume': Parameter(
-            1.2 * units.fL,
-            'Model',
-        ),
-        'initial_area': Parameter(
-            4.52 * units.um**2,
-            'Model',
-        ),
-    },
-    'concs': {
-        'initial_pump': Parameter(
-            6.7e-4 * units.mM,
-            'Model',
-        ),
-        'initial_hydrolase': Parameter(
-            7.1e-4 * units.mM,
-            'Model',
-        ),
-    },
-}
 
 
 def make_beta_lactam_reactions(
@@ -290,29 +188,27 @@ def make_beta_lactam_reactions(
 
 
 def main() -> None:
-    params = ParameterStore(PARAMS)
-
     crn, diffusion_only_crn = make_beta_lactam_reactions(
         'ampicillin',
         MichaelisMentenKinetics(
-            params.get(('ampicillin', 'efflux', 'kcat')),
-            params.get(('ampicillin', 'efflux', 'km')),
-            params.get(('ampicillin', 'efflux', 'n')),
+            param_store.get(('ampicillin', 'efflux', 'kcat')),
+            param_store.get(('ampicillin', 'efflux', 'km')),
+            param_store.get(('ampicillin', 'efflux', 'n')),
         ),
         MichaelisMentenKinetics(
-            params.get(('ampicillin', 'hydrolysis', 'kcat')),
-            params.get(('ampicillin', 'hydrolysis', 'km')),
-            params.get(('ampicillin', 'hydrolysis', 'n')),
+            param_store.get(('ampicillin', 'hydrolysis', 'kcat')),
+            param_store.get(('ampicillin', 'hydrolysis', 'km')),
+            param_store.get(('ampicillin', 'hydrolysis', 'n')),
         ),
-        params.get(('shape', 'initial_area')),
-        params.get(('ampicillin', 'permeability', 'outer')),
+        param_store.get(('shape', 'initial_area')),
+        param_store.get(('ampicillin', 'permeability', 'outer')),
         (
-            params.get(('shape', 'initial_cell_volume'))
-            * params.get(('shape', 'periplasm_fraction'))
+            param_store.get(('shape', 'initial_cell_volume'))
+            * param_store.get(('shape', 'periplasm_fraction'))
         ),
-        params.get(('ampicillin', 'mic')),
-        params.get(('concs', 'initial_pump')),
-        params.get(('concs', 'initial_hydrolase')),
+        param_store.get(('ampicillin', 'mic')),
+        param_store.get(('concs', 'initial_pump')),
+        param_store.get(('concs', 'initial_hydrolase')),
     )
 
     # SAVE SBML FILES.
