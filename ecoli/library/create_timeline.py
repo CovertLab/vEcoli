f1eceab780c4a2f63829fe246b9f8b0ebfda536b
mica <michaelyang42@gmail.com>
2022-07-04 01:01:05 -0600

    adding computed values to timeline

--- ecoli/library/create_timeline.py
+++ ecoli/library/create_timeline.py
@@ -27,6 +27,16 @@ def create_timeline_from_df(df, column_path_mapping, time_column="Time"):
     return result
 
 
+def add_computed_value(timeline, func):
+    """func: (time, values) => dict of calculated values"""
+    return {
+        "timeline": [
+            (time, {**values, **func(time, values)})
+            for time, values in timeline["timeline"]
+        ]
+    }
+
+
 def test_add_timeline():
     TEST_FILE = "data/cell_wall/test_murein_21_06_2022_17_42_11.csv"
     timeline = create_timeline_from_csv(
@@ -36,6 +46,10 @@ def test_add_timeline():
             "CPLX0-7717[m]": ("bulk", "CPLX0-7717[m]"),
             "CPLX0-3951[i]": ("bulk", "CPLX0-3951[i]"),
         },
+    )
+
+    timeline = add_computed_value(
+        timeline, lambda t, val: {"murein*4": val[("bulk", "CPD-12261[p]")] * 4}
     )
 
     print(timeline)
