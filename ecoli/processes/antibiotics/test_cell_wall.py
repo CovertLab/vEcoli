f615871a03442344412713ab1411ac9d0774d076
mica <michaelyang42@gmail.com>
2022-07-05 16:58:58 -0600

    move testing out of cell wall process, bugfixes

--- ecoli/processes/antibiotics/test_cell_wall.py
+++ ecoli/processes/antibiotics/test_cell_wall.py
@@ -5,6 +5,7 @@ from ecoli.library.schema import bulk_schema
 from ecoli.processes.antibiotics.cell_wall import CellWall
 from vivarium.core.composition import add_timeline, simulate_composite
 from vivarium.core.process import Process
+from vivarium.library.units import units
 from vivarium.plots.simulation_output import plot_variables
 
 DATA = "data/cell_wall/test_murein_21_06_2022_17_42_11.csv"
@@ -19,7 +20,13 @@ def create_composite():
             "CPLX0-3951[i]": ("bulk", "CPLX0-3951[i]"),
         },
     )
-    timeline = add_computed_value(timeline, lambda t, value: {})
+    timeline = add_computed_value(
+        timeline,
+        lambda t, value: {
+            ("murein_state", "incorporated_murein"): value[("bulk", "CPD-12261[p]")],
+            ("cell_global", "volume"): (1 + t / 1000),
+        },
+    )
 
     # stub process to ensure updaters exist
     class Stub(Process):
@@ -30,13 +37,30 @@ def create_composite():
             return {
                 "bulk_murein": bulk_schema(["CPD-12261[p]"]),
                 "PBP": bulk_schema(["CPLX0-7717[m]", "CPLX0-3951[i]"]),
+                "murein_state": bulk_schema(
+                    ["incorporated_murein", "unincorporated_murein"]
+                ),
+                "shape": {
+                    "volume": {
+                        "_default": 0,
+                        "_updater": "set",
+                        "_emit": True,
+                    }
+                },
             }
 
         def next_update(self, timestep, states):
             return {}
 
     processes = {"stub": Stub({})}
-    topology = {"stub": {"bulk_murein": ("bulk",), "PBP": ("bulk",)}}
+    topology = {
+        "stub": {
+            "bulk_murein": ("bulk",),
+            "PBP": ("bulk",),
+            "murein_state": ("murein_state",),
+            "shape": ("cell_global"),
+        }
+    }
 
     add_timeline(processes, topology, timeline)
 
@@ -50,6 +74,8 @@ def output_data(data, filepath="out/processes/cell_wall/test_cell_wall.png"):
             ("bulk", "CPD-12261[p]"),
             ("bulk", "CPLX0-7717[m]"),
             ("bulk", "CPLX0-3951[i]"),
+            ("murein_state", "incorporated_murein"),
+            # ("murein_state", "unincorporated_murein")
         ],
         out_dir=os.path.dirname(filepath),
         filename=os.path.basename(filepath),
@@ -57,9 +83,13 @@ def output_data(data, filepath="out/processes/cell_wall/test_cell_wall.png"):
 
 
 def test_cell_wall():
-    composite, initial_state = create_composite()
+    composite = create_composite()
 
-    settings = {"initial_state": initial_state, "return_raw_data": False, "total_time": 500}
+    settings = {
+        "initial_state": {},
+        "return_raw_data": False,
+        "total_time": 500,
+    }
     data = simulate_composite(composite, settings)
 
     output_data(data)
