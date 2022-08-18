52d5c93f57fa744b139f4a53f5febf5551f15494
cs <cs.temporary@icloud.com>
2022-08-18 16:15:04 +0000

    Speed up multigen analysis by filtering data by path

--- ecoli/analysis/analyze_multigen_db_experiment.py
+++ ecoli/analysis/analyze_multigen_db_experiment.py
@@ -2,10 +2,12 @@ import argparse
 
 from vivarium.core.serialize import deserialize_value
 from vivarium.plots.agents_multigen import plot_agents_multigen
-from vivarium.library.topology import get_in, assoc_path
+from vivarium.library.topology import (
+    get_in, assoc_path, convert_path_style)
 from vivarium.library.units import remove_units
 
-from ecoli.analysis.analyze_db_experiment import access, OUT_DIR
+from ecoli.analysis.db import access, get_agent_ids
+from ecoli.analysis.analyze_db_experiment import OUT_DIR
 
 AGENTS_PATH = ('agents',)
 SKIP_PATHS = [
@@ -19,17 +21,34 @@ def main():
     parser = argparse.ArgumentParser(
         description='Plot data from multigen experiment.')
     parser.add_argument(
-        '--experiment_id', '-e',
-        type=str, default='')
+        'experiment_id', type=str)
     parser.add_argument(
         '--host', '-o', default='localhost', type=str)
     parser.add_argument(
         '--port', '-p', default=27017, type=int)
+    parser.add_argument(
+        '--path', '-t', type=str, nargs='*', default=[])
+    parser.add_argument(
+        '--agent', '-a', type=str, nargs='*', default=[])
     args = parser.parse_args()
+
+    agents = get_agent_ids(
+        args.experiment_id, args.host, args.port)
+    if args.agent:
+        assert set(args.agent) - agents == set()
+        agents = args.agent
+
+    paths = [convert_path_style(path) for path in args.path]
+    query = []
+    for path in paths:
+        for agent in agents:
+            query.append(('agents', agent) + path)
+    if not query:
+        query = None
 
     # Retrieve all simulation data.
     data, experiment_id, sim_config = access(
-        args.experiment_id, host=args.host, port=args.port)
+        args.experiment_id, query=query, host=args.host, port=args.port)
     data = deserialize_value(data)
     data = remove_units(data)
 
