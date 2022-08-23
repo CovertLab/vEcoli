777b6d95b583b0d7bc9adb0292e11f85ea8a7c6a
mica <michaelyang42@gmail.com>
2022-08-23 16:39:25 -0400

    simplify and remove unecessary code, work towards fixing strand length issues

--- ecoli/library/cell_wall/column_sampler.py
+++ ecoli/library/cell_wall/column_sampler.py
@@ -1,12 +1,7 @@
-import argparse
-import os
-from collections import Counter
-from itertools import accumulate, chain, takewhile, tee
-from re import S
+from itertools import accumulate, takewhile, tee
 
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import arg
 
 
 def poisson_sampler(rng, rate):
@@ -66,64 +61,19 @@ def sample_column(rows, murein, strand_sampler, rng, shift=True):
     return result
 
 
-def sample_lattice(rows, columns, murein, strand_sampler, rng):
-    result = np.zeros((rows, columns), dtype=int)
+def sample_lattice(murein_monomers, rows, cols, strand_sampler, rng):
+    result = np.zeros((rows, cols), dtype=int)
 
     # Get murein in each column, distributing extra murein uniformly at random
-    murein_per_column = np.repeat(murein // columns, repeats=columns)
-    extra_murein = murein % columns
-    for col in rng.integers(0, columns, size=extra_murein):
+    murein_per_column = np.repeat(murein_monomers // cols, repeats=cols)
+    extra_murein = murein_monomers % cols
+    for col in rng.integers(0, cols, size=extra_murein):
         murein_per_column[col] += 1
 
     for c, murein in enumerate(murein_per_column):
         result[:, c] = sample_column(rows, murein, strand_sampler, rng)
 
     return result
-
-
-def length_distributions(column):
-    strand_lengths = []
-    gap_lengths = []
-
-    in_strand = bool(column[0])
-    current_length = 0
-    for x in column:
-        if x == int(in_strand):
-            current_length += 1
-        else:
-            (strand_lengths if in_strand else gap_lengths).append(current_length)
-            in_strand = not in_strand
-            current_length = 1
-
-    return strand_lengths, gap_lengths
-
-
-def plot_length_distributions(strand_lengths, gap_lengths):
-    fig, axs = plt.subplots(nrows=1, ncols=2)
-    for c, data in enumerate([strand_lengths, gap_lengths]):
-        flat_data = list(chain(*data))
-        N = len(flat_data)
-
-        # Create histogram of each run in the data
-        histograms = np.zeros((len(data), max(flat_data) + 1))
-        for run, run_data in enumerate(data):
-            hist = Counter(run_data)
-            for k, v in hist.items():
-                histograms[run, k] = v
-
-        # Normalize as probability distributions
-        distributions = histograms / histograms.sum(axis=1)[:, np.newaxis]
-
-        # Calculate means, error bars
-        y = distributions.mean(axis=0)
-        err = distributions.std(axis=0) / np.sqrt(N)
-        bincenters = np.arange(y.size)
-        axs[c].bar(bincenters, y, yerr=err)
-        axs[c].set_xticks(bincenters)
-        axs[c].set_xticklabels(axs[c].get_xticks(), rotation=45)
-        axs[c].set_title(f"{('Strand', 'Gap')[c]} Length Distribution")
-
-    return fig, axs
 
 
 def plot_locational(columns):
@@ -145,28 +95,19 @@ def plot_locational(columns):
 
 def test_column_sampler(outdir="out/murein_sampling"):
     rng = np.random.default_rng(0)
+    p = .058
 
     columns = []
-    strand_lengths = []
-    gap_lengths = []
 
     rows = 3050
-    cols = 700
-    murein = 401898
+    cols = 599
+    murein = 450000
     for i in range(cols):
-        col = sample_column(rows, int(murein // cols), geom_sampler(rng, 0.058), rng)
-        s, g = length_distributions(col)
+        col = sample_column(rows, int(murein // cols), geom_sampler(rng, p), rng)
         columns.append(col)
-        strand_lengths.append(s)
-        gap_lengths.append(g)
 
     # Diagnostic plots ====================================================
     os.makedirs(outdir, exist_ok=True)
-
-    fig, _ = plot_length_distributions(strand_lengths, gap_lengths)
-    fig.set_size_inches((10, 6))
-    fig.tight_layout()
-    fig.savefig(os.path.join(outdir, "length_distributions.png"))
 
     fig, _ = plot_locational(columns)
     fig.set_size_inches((8, 6))
@@ -175,38 +116,7 @@ def test_column_sampler(outdir="out/murein_sampling"):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Sample lattice and run tests.")
-
-    parser.add_argument("--test", "-t", action="store_true", help="Run tests.")
-    parser.add_argument(
-        "--seed",
-        "-s",
-        type=int,
-        default=None,
-        help="Random seed for sampling (not fixed if None)",
-    )
-
-    parser.add_argument("rows", type=int, help="Number of rows to sample.")
-    parser.add_argument("cols", type=int, help="Number of cols to sample.")
-    parser.add_argument("murein", type=int, help="Murein limitation.")
-    parser.add_argument("rate", type=float, help="Rate for poisson distribution")
-    parser.add_argument("--file", "-f", type=str, default=None, help="File to output to.")
-
-    args = parser.parse_args()
-
-    if args.test:
-        test_column_sampler()
-    else:
-        rng = np.random.default_rng(args.seed)
-        lat = sample_lattice(
-            args.rows, args.cols, args.murein, poisson_sampler(rng, args.rate), rng
-        )
-        if args.file:
-            with open(args.file, "w") as f:
-                f.write("[")
-                for row in lat:
-                    f.write(f'{list(row)},\n')
-                f.write("]\n")
+    test_column_sampler()
 
 
 if __name__ == "__main__":
