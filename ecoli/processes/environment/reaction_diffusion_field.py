18773eed39d8b3e9c02343390084180ac30bedaf
Sean Cheah <cheah_sean@yahoo.com>
2022-09-17 09:21:06 -0700

    Clean up and fix emitter handling

--- ecoli/processes/environment/reaction_diffusion_field.py
+++ ecoli/processes/environment/reaction_diffusion_field.py
@@ -5,7 +5,6 @@ Reaction Diffusion Field
 '''
 import copy
 import os
-import psutil
 import numpy as np
 from scipy import constants
 from scipy.ndimage import convolve
@@ -226,8 +225,6 @@ class ReactionDiffusion(Process):
             'agents': local_environments}
         
         deep_merge(update['agents'], agent_updates)
-
-        print(os.getpid(), psutil.Process(os.getpid()).memory_full_info().pss / 1024 ** 2)
 
         return update
 
