02af460017fc46e6198ed8d1db310481aa06b6d0
mica <michaelyang42@gmail.com>
2022-09-24 23:11:17 -0700

    switch to using mean lysis time

--- ecoli/processes/antibiotics/lysis_initiation.py
+++ ecoli/processes/antibiotics/lysis_initiation.py
@@ -10,7 +10,7 @@ from ecoli.processes.registries import topology_registry
 NAME = "ecoli-lysis-initiation"
 TOPOLOGY = {
     "cracked": ("wall_state", "cracked"),
-    "lysis_trigger": None  # TODO: connect with Lysis
+    "lysis_trigger": None,  # TODO: connect with Lysis
 }
 topology_registry.register(NAME, TOPOLOGY)
 
@@ -19,16 +19,16 @@ class LysisInitiation(Process):
     name = NAME
 
     defaults = {
-        "lysis_rate": param_store.get(("lysis_initiation", "lysis_rate")),
+        "mean_lysis_time": param_store.get(("lysis_initiation", "lysis_rate")),
         "seed": 0,
     }
 
     def __init__(self, parameters=None):
         super().__init__(parameters)
 
-        lysis_rate = self.parameters["lysis_rate"]
+        mean_lysis_time = self.parameters["mean_lysis_time"]
         rng = np.random.default_rng(self.parameters["seed"])
-        self.remaining_time = rng.exponential(1 / lysis_rate)
+        self.remaining_time = rng.exponential(mean_lysis_time)
 
     def ports_schema(self):
         return {"cracked": {"_default": False}, "lysis_trigger": {"_default": True}}
@@ -40,3 +40,15 @@ class LysisInitiation(Process):
                 return {"lysis_trigger": True}
 
         return {}
+
+
+def test_lysis_initiation():
+    lysis_initiation = LysisInitiation()
+
+
+def main():
+    test_lysis_initiation()
+
+
+if __name__ == "__main__":
+    main()
