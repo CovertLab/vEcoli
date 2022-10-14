e9379b4614c9ead7e9ca5df45386050a6f6bf896
thalassemia <67928790+thalassemia@users.noreply.github.com>
2022-10-14 11:14:41 -0700

    Consolidate colony plot code

--- ecoli/analysis/antibiotics_colony/snapshot_stripbox.py
+++ ecoli/analysis/antibiotics_colony/snapshot_stripbox.py
@@ -1,20 +1,13 @@
-import os
-import argparse
-from typing import Union, Tuple, List
+from typing import Union, Tuple
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from ecoli.analysis.antibiotics_colony.plot_utils import (
-    retrieve_data,
-    get_config_names
-)
-
 Color = Union[str, Tuple[float, float, float, float]]
 
 
-def plot_boxstrip(
+def plot_stripbox(
     data: pd.DataFrame,
     out: str
 ) -> None:
@@ -43,94 +36,3 @@ def plot_boxstrip(
             g.savefig('out/analysis/antibiotics_colony/' + 
                 f'{out}_{column.replace("/", "_")}.png')
             plt.close(g)
-
-
-def make_stripbox_plots(
-    baseline_configs: List[str],
-    exp_configs: List[str],
-    baseline_colors: List[str],
-    exp_colors: List[str],
-    sampling_rate: int,
-    cpus: int = 8,
-    out: str = 'stripbox'
-):
-    """Helper function to retrieve and plot timeseries data.
-    
-    Args:
-        baseline_configs: List of JSON filenames to configure
-            baseline data retrieval (each is a replicate).
-        exp_configs: List of JSON filenames to configure
-            experimental data retrieval (each is a replicate).
-        baseline_colors: Hex colors for baseline replicates.
-        exp_colors: Hex colors for experimental replicates.
-        div_and_death: Mark death and division on plots (X and +)
-        cpus: Number of CPU cores to use.
-        out: Prefix for output plot filenames
-    """
-    data = retrieve_data(
-        configs=baseline_configs,
-        colors=baseline_colors, 
-        sampling_rate=sampling_rate,
-        cpus=cpus)
-    exp_data = retrieve_data(
-        configs=exp_configs,
-        colors=exp_colors, 
-        sampling_rate=sampling_rate,
-        cpus=cpus)
-    data = pd.concat([data, exp_data], ignore_index=True)
-    os.makedirs('out/analysis/antibiotics_colony/', exist_ok=True)
-    plot_boxstrip(data, out)
-
-
-def main():
-    sns.set_style("white")
-    amp_configs = get_config_names('ampicillin')
-    tet_configs = get_config_names('tetracycline')
-    glc_tet_configs = get_config_names('glucose_tet')
-    glc_amp_configs = get_config_names('glucose_amp')
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--tetracycline', '-t', type=bool, default=True,
-        help="""Compare tetracycline sims with glucose sims."""
-    )
-    parser.add_argument(
-        '--ampicillin', '-a', type=bool, default=True,
-        help="""Compare ampicillin sims with glucose sims."""
-    )
-    parser.add_argument(
-        '--division_and_death', '-d', type=bool, default=True,
-        help="""Mark values for agents right before cell division with
-            a "+" sign. Mark values for agents right before cell death
-            with a "x" sign."""
-    )
-    parser.add_argument(
-        '--cpus', '-p', type=int, default=1,
-        help="""Number of CPU cores to use."""
-    )
-    args = parser.parse_args()
-    # Shades of grey for baseline distributions (up to 3 replicates)
-    baseline_colors = ('#333333', '#777777', '#BBBBBB')
-    # Shades of blue-green for experimental distributions (up to 3 replicates)
-    colors = ('#5F9EA0', '#088F8F', '#008080')
-    if args.tetracycline:
-        make_stripbox_plots(
-            baseline_configs=glc_tet_configs,
-            exp_configs=tet_configs,
-            baseline_colors=baseline_colors,
-            exp_colors=colors,
-            div_and_death=args.division_and_death,
-            cpus=args.cpus,
-            out='tetracycline_sb')
-    if args.ampicillin:
-        make_stripbox_plots(
-            baseline_configs=glc_amp_configs,
-            exp_configs=amp_configs,
-            baseline_colors=baseline_colors,
-            exp_colors=colors,
-            div_and_death=args.division_and_death,
-            cpus=args.cpus,
-            out='ampicillin_sb')
-
-
-if __name__ == "__main__":
-    main()
