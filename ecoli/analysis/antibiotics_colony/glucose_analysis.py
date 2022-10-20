3c3138128799b1381c4d7a2d4acc0c33e3f894f3
mica <michaelyang42@gmail.com>
2022-10-19 22:00:10 -0700

    remove section option

--- ecoli/analysis/antibiotics_colony/glucose_analysis.py
+++ ecoli/analysis/antibiotics_colony/glucose_analysis.py
@@ -132,39 +132,16 @@ def get_data(experiment_ids, sampling_rate, start_time, end_time, host, port, cp
     return experiment_data, bounds
 
 
-def make_figure(data, bounds, section=None):
-    if section is None:
-        section = ["A", "B", "C", "D", "E", "F"]
-
+def make_figure(data, bounds):
     fig, axs = make_layout(width=8, height=8)
 
-    if "A" in section:
-        make_figure_A(fig, [ax for k, ax in axs.items() if k.startswith("A")], data. bounds)
-    if "B" in section:
-        pass
-    if "C" in section:
-        pass
-    if "D" in section:
-        pass
-    if "E" in section:
-        pass
-    if "F" in section:
-        pass
+    make_figure_A(fig, [ax for k, ax in axs.items() if k.startswith("A")], data. bounds)
 
     fig.savefig("out/glucose_figure.png")
 
 
 def main():
     parser = argparse.ArgumentParser("Generate glucose figures.")
-
-    parser.add_argument(
-        "--section",
-        "-s",
-        default=None,
-        choices=[None, "A", "B", "C", "D", "E", "F"],
-        help="Generate a particular sub-figure (defaults to generating all figures)",
-        required=False,
-    )
 
     parser.add_argument(
         "--experiment_ids",
@@ -199,7 +176,7 @@ def main():
         port=args.port,
         cpus=args.cpus,
     )
-    make_figure(data, bounds, args.section)
+    make_figure(data, bounds)
 
 
 if __name__ == "__main__":
