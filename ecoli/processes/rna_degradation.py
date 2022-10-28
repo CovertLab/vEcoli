81a249a764d973ac12f43dc2bec00b8947f11cf2
Sean Cheah <cheah_sean@yahoo.com>
2022-10-28 09:48:18 -0700

    Only calculate miscRNA degradation when non-zero

--- ecoli/processes/rna_degradation.py
+++ ecoli/processes/rna_degradation.py
@@ -322,16 +322,18 @@ class RnaDegradation(PartitionedProcess):
             
             # NEW to vivarium-ecoli
             # Degrade miscRNAs, including RNA duplexes from RNA interference
-            miscrna_specificity = np.dot(frac_endornase_saturated, self.is_miscRNA)
-            n_total_miscrnas_to_degrade = self._calculate_total_n_to_degrade(
-                timestep,
-                miscrna_specificity,
-                total_kcat_endornase)
-            miscrna_deg_probs = 1. / np.dot(self.is_miscRNA, rna_exists) * self.is_miscRNA * rna_exists
-            miscrna_counts = total_RNA_counts * self.is_miscRNA
-            n_miscrnas_to_degrade = self._get_rnas_to_degrade(
-                n_total_miscrnas_to_degrade, miscrna_deg_probs, miscrna_counts)
-            n_RNAs_to_degrade = n_RNAs_to_degrade + n_miscrnas_to_degrade
+            n_existing_miscrnas = np.dot(self.is_miscRNA, rna_exists)
+            if n_existing_miscrnas > 0:
+                miscrna_specificity = np.dot(frac_endornase_saturated, self.is_miscRNA)
+                n_total_miscrnas_to_degrade = self._calculate_total_n_to_degrade(
+                    timestep,
+                    miscrna_specificity,
+                    total_kcat_endornase)
+                miscrna_deg_probs = 1. / n_existing_miscrnas * self.is_miscRNA * rna_exists
+                miscrna_counts = total_RNA_counts * self.is_miscRNA
+                n_miscrnas_to_degrade = self._get_rnas_to_degrade(
+                    n_total_miscrnas_to_degrade, miscrna_deg_probs, miscrna_counts)
+                n_RNAs_to_degrade = n_RNAs_to_degrade + n_miscrnas_to_degrade
 
         # First order decay with non-functional EndoRNase activity
         # Determine mRNAs to be degraded by sampling a Poisson distribution
