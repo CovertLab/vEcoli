5f63d893e1112d688a8dc2c1b6e87b2898b0169c
thalassemia <67928790+thalassemia@users.noreply.github.com>
2022-11-08 17:15:12 -0800

    More work on Figure 3

--- ecoli/analysis/antibiotics_colony/timeseries.py
+++ ecoli/analysis/antibiotics_colony/timeseries.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any
 import itertools
 
+import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -10,30 +11,7 @@ from matplotlib.colors import rgb_to_hsv
 from ecoli.plots.snapshots import plot_snapshots, plot_tags
 
 from ecoli.analysis.antibiotics_colony import (
-    MAX_TIME, SPLIT_TIME, COUNTS_PER_FL_TO_NANOMOLAR)
-
-
-def restrict_data(data: pd.DataFrame):
-    """If there is more than one condition in data, keep up
-    to SPLIT_TIME from the first condition and between SPLIT_TIME
-    and MAX_TIME from the second."""
-    conditions = data.loc[:, 'Condition'].unique()
-    if len(conditions) > 1:
-        data = data.set_index(['Condition'])
-        condition_1_mask = ((data.loc[conditions[0]]['Time'] 
-            <= SPLIT_TIME))
-        condition_1_data = data.loc[conditions[0]].loc[
-            condition_1_mask, :]
-        condition_2_mask = ((data.loc[conditions[1]]['Time'] 
-            >= SPLIT_TIME) & (data.loc[conditions[1]]['Time'] 
-            <= MAX_TIME))
-        condition_2_data = data.loc[conditions[1]].loc[
-            condition_2_mask, :]
-        data = pd.concat([condition_1_data, condition_2_data])
-        data = data.reset_index()
-    else:
-        data = data.loc[data.loc[:, 'Time'] <= MAX_TIME, :]
-    return data
+    COUNTS_PER_FL_TO_NANOMOLAR, restrict_data)
 
 
 def plot_timeseries(
@@ -44,6 +22,10 @@ def plot_timeseries(
     conc: bool = False,
     mark_death: bool = False,
     background_lineages: bool = True,
+    filter_time: bool = True,
+    background_color: tuple = (0.5, 0.5, 0.5),
+    background_alpha: float = 0.5,
+    background_linewidth: float = 0.1
 ) -> None:
     '''Plot selected traces with specific lineage highlighted and others in gray.
 
@@ -52,7 +34,9 @@ def plot_timeseries(
             of interest. Must have these columns: 'Time', 'Death', 'Agent ID',
             'Boundary', 'Condition', and 'Seed'. The first experimental condition
             in the 'Condition' column is treated as a control and plotted in gray.
-            Include at most 2 conditions and 1 seed per condition.
+            Include at most 2 conditions and 1 seed per condition. If more than 1
+            condition is supplied, either ensure that they do not share any time
+            points or run with the ``restrict_data`` option set to true.
         axes: Columns are plotted sequentially on these Axes.
         columns_to_plot: Dictionary of columns in data to plot sequentially on
             axes. Each column name corresponds to a RGB tuple to color the trace
@@ -61,6 +45,12 @@ def plot_timeseries(
         conc: Whether to normalize data by volume and convert to nM
         mark_death: Mark cells that die with red X on time step of death
         background_lineages: Whether to plot traces for other lineages (gray).
+        filter_time: Apply default time filter for ``data`` (take first 11550
+            seconds from assumed control condition and 11550-26000 seconds from
+            all other conditions)
+        background_color: Color used to plot traces for non-highlighted agents
+        background_alpha: Alpha used to plot traces for non-highlighted agents
+        background_linewidth: Linewidth for non-highlighted agent traces
     '''
     columns_to_include = list(columns_to_plot.keys() |
         {'Agent ID', 'Condition', 'Time'})
@@ -82,8 +72,9 @@ def plot_timeseries(
     background_data = data.loc[~lineage_mask, :]
     # Plot up to SPLIT_TIME with first condition and between SPLIT_TIME
     # and MAX_TIME with second condition
-    background_data = restrict_data(background_data)
-    highlight_data = restrict_data(highlight_data)
+    if filter_time:
+        background_data = restrict_data(background_data)
+        highlight_data = restrict_data(highlight_data)
     
     # Convert time to hours
     highlight_data.loc[:, 'Time'] /= 3600
@@ -117,8 +108,8 @@ def plot_timeseries(
         if background_lineages:
             for _, background_agent in background_data:
                 curr_ax.plot(background_agent.loc[:, 'Time'],
-                    background_agent.loc[:, column], c=(0.5, 0.5, 0.5),
-                    linewidth=0.1)
+                    background_agent.loc[:, column], c=background_color,
+                    linewidth=background_linewidth, alpha=background_alpha)
         for _, highlight_agent in highlight_data:
             curr_ax.plot(highlight_agent.loc[:, 'Time'],
                 highlight_agent.loc[:, column], c=color,
@@ -211,13 +202,14 @@ def plot_field_snapshots(
         scale_bar_length=10,
         membrane_color=(0, 0, 0),
         membrane_width=0.01,
-        colorbar_decimals=1
+        colorbar_decimals=1,
+        default_font_size=48
     )
     snapshots_fig.subplots_adjust(wspace=0.7, hspace=0.1)
     snapshots_fig.axes[1].set_ylabel(None)
     snapshots_fig.axes[-2].set_title(None)
     snapshots_fig.axes[-1].set_title(
-        'External Glucose\n(mM)', y=1.1, fontsize=36)
+        'External Glucose\n(mM)', y=1.1, fontsize=48)
     snapshots_fig.axes[0].set_xticklabels(snapshot_times_hrs)
     snapshots_fig.axes[0].set_xlabel('Time (hr)')
     snapshots_fig.savefig('out/analysis/paper_figures/' + 
@@ -229,11 +221,12 @@ def plot_field_snapshots(
 def plot_tag_snapshots(
     data: pd.DataFrame,
     metadata: Dict[str, Dict[int, Dict[str, Any]]],
-    highlight_column: str = None,
-    highlight_color: tuple = (1, 0, 0),
+    tag_colors: Dict[str, Any] = None,
     conc: bool = False,
     snapshot_times: List = None,
     min_color: Any = None,
+    out_prefix: str = None,
+    show_membrane: bool = False,
 ) -> None:
     '''Plot a row of snapshot images that span a replicate for each condition.
     In each of these images, cells will be will be colored with highlight_color
@@ -248,72 +241,91 @@ def plot_tag_snapshots(
             following keys: 'bounds' is of the form [x, y] and gives the dimensions
             of the spatial environment and 'fields' is a dictionary timeseries of
             the 'fields' Store for that condition and initial seed
-        highlight_column: Variable to use to color cells
-        highlight_color: Color for cells with highest highlight_column value
+        tag_colors: Mapping column names in ``data`` to either RGB tuples or
+            dictionaries containing the ``cmp`` and ``norm`` keys for the 
+            :py:class:`matplotlib.colors.Colormap` and 
+            :py:class:`matplotlib.colors.Normalize` instances to use for that tag
+            If dictionaries are used, the ``min_color`` key is overrriden
         conc: Whether to normalize by volume before plotting
         snapshot_times: Times (in seconds) to make snapshots for
         min_color: Color for cells with lowest highlight_column value (default white)
+        out_prefix: Prefix for output filename
+        show_membrane: Whether to draw outline for agents
     '''
-    if snapshot_times is None:
-        # Last snapshot at last tenth of an hour
-        max_time_hrs = np.around(data.loc[:, 'Time'].max()/3600, decimals=1)
-        snapshot_times_hrs = np.around(np.linspace(0, max_time_hrs, 5),
-            decimals=1)
-        snapshot_times = snapshot_times_hrs * 3600
-        # Use 10 seconds for first snapshot to include cell wall update
-        snapshot_times[0] = 10
-    else:
-        snapshot_times_hrs = snapshot_times / 3600
-    data = pd.concat([
-        data.loc[data['Time']==time, :] for time in snapshot_times
-    ])
-    # Get first SPLIT_TIME seconds from condition #1 and rest from condition #2
-    data = restrict_data(data)
-    # Sort values by time for ease of plotting later
-    data = data.sort_values('Time')
-    condition = '_'.join(data.loc[:, 'Condition'].unique())
-    seed = data.loc[:, 'Seed'].unique()[0]
-    if conc:
-        # Convert to concentrations
-        data = data.set_index(['Condition', 'Time', 'Agent ID'])
-        data = data.divide(data['Volume'], axis=0).drop(['Volume'], axis=1)
-        data = data * COUNTS_PER_FL_TO_NANOMOLAR
-        data = data.reset_index()
-    condition_bounds = metadata[min(metadata)][seed]['bounds']
-    # Convert data back to dictionary form for snapshot plot
-    snapshot_data = {}         
-    for time, agent_id, boundary, column in zip(
-        data['Time'], data['Agent ID'], data['Boundary'],
-        data[highlight_column]
-    ):
-        data_at_time = snapshot_data.setdefault(time, {})
-        agents_at_time = data_at_time.setdefault('agents', {})
-        agent_at_time = agents_at_time.setdefault(agent_id, {})
-        agent_at_time['boundary'] = boundary
-        agent_at_time[highlight_column] = column
-    snapshots_fig = plot_tags(
-        data=snapshot_data,
-        bounds=condition_bounds,
-        scale_bar_length=5,
-        membrane_width=0,
-        colorbar_decimals=1,
-        background_color='white',
-        min_color=min_color,
-        tag_colors={(highlight_column,): rgb_to_hsv(highlight_color)},
-        tagged_molecules=[(highlight_column,)],
-        default_font_size=36,
-        convert_to_concs=False,
-        tag_path_name_map={(highlight_column,): highlight_column},
-        xlim=[15, 35],
-        ylim=[15, 35],
-    )
-    snapshots_fig.subplots_adjust(wspace=0.7, hspace=0.1)
-    snapshots_fig.axes[1].set_ylabel(None)
-    snapshots_fig.axes[-1].set_title(None)
-    snapshots_fig.axes[-1].set_title(highlight_column, fontsize=36, y=1.05)
-    snapshots_fig.axes[0].set_xticklabels(snapshot_times_hrs, fontsize=36)
-    snapshots_fig.axes[0].set_xlabel('Time (hr)', fontsize=36)
-    snapshots_fig.savefig('out/analysis/paper_figures/' + 
-        f'{condition.replace("/", "_")}_seed_{seed}_tags.svg',
-        bbox_inches='tight')
-    plt.close(snapshots_fig)
+    for highlight_column, tag_color in tag_colors.items():
+        if snapshot_times is None:
+            # Last snapshot at last tenth of an hour
+            max_time_hrs = np.around(data.loc[:, 'Time'].max()/3600, decimals=1)
+            snapshot_times_hrs = np.around(np.linspace(0, max_time_hrs, 5),
+                decimals=1)
+            snapshot_times = snapshot_times_hrs * 3600
+            # Use 10 seconds for first snapshot to include cell wall update
+            snapshot_times[0] = 10
+        else:
+            snapshot_times_hrs = snapshot_times / 3600
+        data = pd.concat([
+            data.loc[data['Time']==time, :] for time in snapshot_times
+        ])
+        # Get first SPLIT_TIME seconds from condition #1 and rest from condition #2
+        data = restrict_data(data)
+        # Sort values by time for ease of plotting later
+        data = data.sort_values('Time')
+        condition = '_'.join(data.loc[:, 'Condition'].unique())
+        seed = data.loc[:, 'Seed'].unique()[0]
+        if conc:
+            # Convert to concentrations
+            data = data.set_index(['Condition', 'Time', 'Agent ID'])
+            data = data.divide(data['Volume'], axis=0).drop(['Volume'], axis=1)
+            data = data * COUNTS_PER_FL_TO_NANOMOLAR
+            data = data.reset_index()
+        condition_bounds = metadata[min(metadata)][seed]['bounds']
+        # Convert data back to dictionary form for snapshot plot
+        snapshot_data = {}
+        magnitude = data[highlight_column].abs().max()
+        tag_ranges = {(highlight_column,): [-magnitude, magnitude]}
+        for time, agent_id, boundary, column in zip(
+            data['Time'], data['Agent ID'], data['Boundary'],
+            data[highlight_column]
+        ):
+            data_at_time = snapshot_data.setdefault(time, {})
+            agents_at_time = data_at_time.setdefault('agents', {})
+            agent_at_time = agents_at_time.setdefault(agent_id, {})
+            agent_at_time['boundary'] = boundary
+            agent_at_time[highlight_column] = column
+        if show_membrane:
+            membrane_width = 0.1
+        else:
+            membrane_width = 0
+        snapshots_fig = plot_tags(
+            data=snapshot_data,
+            bounds=condition_bounds,
+            scale_bar_length=5,
+            membrane_width=membrane_width,
+            membrane_color=(0, 0, 0),
+            colorbar_decimals=1,
+            background_color='white',
+            min_color=min_color,
+            tag_colors={(highlight_column,): tag_color},
+            tagged_molecules=[(highlight_column,)],
+            default_font_size=48,
+            convert_to_concs=False,
+            tag_path_name_map={(highlight_column,): highlight_column},
+            xlim=[15, 35],
+            ylim=[15, 35],
+            n_snapshots=len(snapshot_times),
+            tag_ranges=tag_ranges
+        )
+        snapshots_fig.subplots_adjust(wspace=0.7, hspace=0.1)
+        snapshots_fig.axes[1].set_ylabel(None)
+        snapshots_fig.axes[-1].set_title(None)
+        snapshots_fig.axes[-1].set_title(highlight_column, fontsize=48, y=1.05)
+        snapshots_fig.axes[0].set_xticklabels(snapshot_times_hrs, fontsize=48)
+        snapshots_fig.axes[0].set_xlabel('Time (hr)', fontsize=48)
+        out_name = f'{condition.replace("/", "_")}_seed_{seed}_tags.svg'
+        out_name = highlight_column.replace("/", "_") + '_' + out_name
+        if out_prefix:
+            out_name  = '_'.join([out_prefix, out_name])
+        out_dir = 'out/analysis/paper_figures/'
+        snapshots_fig.savefig(os.path.join(out_dir, out_name),
+            bbox_inches='tight')
+        plt.close(snapshots_fig)
