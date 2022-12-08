ee7c3ba6465fe5025d1f3cd2477d58d9d5405c76
thalassemia <67928790+thalassemia@users.noreply.github.com>
2022-12-07 16:27:32 -0800

    More antibiotic CLI options

--- ecoli/experiments/tet_amp_sim.py
+++ ecoli/experiments/tet_amp_sim.py
@@ -10,7 +10,8 @@ from migration.migration_utils import recursive_compare
 from ecoli.library.parameters import param_store
 
 def run_sim(tet_conc=0, amp_conc=0, baseline=False, seed=0,
-    cloud=False, initial_colony_file=None, start_time=0, runtime=None,
+    cloud=False, initial_colony_file=None, initial_state_file=None,
+    start_time=0, runtime=None,
     ):
     config = SimConfig()
     config.update_from_json(os.path.join(
@@ -18,12 +19,12 @@ def run_sim(tet_conc=0, amp_conc=0, baseline=False, seed=0,
     config.update_from_json(os.path.join(
         CONFIG_DIR_PATH, "antibiotics_tetracycline.json"))
     add_opts = {
-        'spatial_environment_config': {
-            'reaction_diffusion': {
-                'gradient': {
-                    'molecules': {
-                        'tetracycline': tet_conc * units.mM,
-                        'ampicillin[p]': amp_conc * units.mM  
+        "spatial_environment_config": {
+            "reaction_diffusion": {
+                "gradient": {
+                    "molecules": {
+                        "tetracycline": tet_conc * units.mM,
+                        "ampicillin[p]": amp_conc * units.mM  
                     }
                 }
             },
@@ -37,26 +38,28 @@ def run_sim(tet_conc=0, amp_conc=0, baseline=False, seed=0,
                 ]
             },
         },
-        'seed': seed,
-        'start_time': start_time,
-        'colony_save_prefix': 'tet_amp',
-        'save': True
+        "seed": seed,
+        "start_time": start_time,
+        "colony_save_prefix": "tet_amp",
     }
     config.update_from_dict(add_opts)
     if initial_colony_file:
-        config._config.pop('initial_state_file')
-        config['initial_colony_file'] = initial_colony_file
+        config._config.pop("initial_state_file")
+        config["initial_colony_file"] = initial_colony_file
+    elif initial_state_file:
+        config["initial_state_file"] = initial_state_file
     else:
-        make_tet_initial_state('wcecoli_t0')
+        make_tet_initial_state("wcecoli_t0")
     if baseline:
-        print(f'Running baseline sim (seed = {seed}).')
+        print(f"Running baseline sim (seed = {seed}).")
         config["colony_save_prefix"] = "glc_combined"
         # 26000 allows 8th round of division to mostly complete
         # Run for one timestep past that to catch inner sim emits at 26000
         if not runtime:
             runtime = 26002
-            config['save_times'] = [11550, 23100]
-        config['total_time'] = runtime
+            config["save"] = True
+            config["save_times"] = [11550, 23100]
+        config["total_time"] = runtime
         # Ensure that sim starts with correctly reduced murein counts
         config["initial_state_overrides"] = ["overrides/reduced_murein"]
     else:
@@ -66,10 +69,11 @@ def run_sim(tet_conc=0, amp_conc=0, baseline=False, seed=0,
         config["colony_save_prefix"] = f"amp_{amp_conc}_tet_{tet_conc}"
         if not runtime:
             runtime = 14452
-            config['save_times'] = [11550]
-        config['total_time'] = runtime
+            config["save"] = True
+            config["save_times"] = [11550]
+        config["total_time"] = runtime
     if cloud:
-        config['emitter_arg'] = [
+        config["emitter_arg"] = [
             ["host", "10.138.0.75:27017"],
             ["emit_limit", 5000000]
         ]
@@ -79,35 +83,36 @@ def run_sim(tet_conc=0, amp_conc=0, baseline=False, seed=0,
 
 def add_mar_tf(data):
     # Add initial count for marR-tet complex
-    data['bulk']['marR-tet[c]'] = 0
+    data["bulk"]["marR-tet[c]"] = 0
     # Add promoter binding data for marA and marR
-    for promoter_data in data['unique']['promoter'].values():
-        promoter_data['bound_TF'] += [False, False]
+    for promoter_data in data["unique"]["promoter"].values():
+        promoter_data["bound_TF"] += [False, False]
     return data
 
 
 def make_tet_initial_state(initial_file):
-    with open(f'data/{initial_file}.json') as f:
+    with open(f"data/{initial_file}.json") as f:
         initial_state = json.load(f)
     # Modify each cell in colony individually
-    if 'agents' in initial_state:
-        for agent_id, agent_data in initial_state['agents'].items():
-            initial_state['agents'][agent_id] = add_mar_tf(agent_data)
+    if "agents" in initial_state:
+        for agent_id, agent_data in initial_state["agents"].items():
+            initial_state["agents"][agent_id] = add_mar_tf(agent_data)
     else:
         initial_state = add_mar_tf(initial_state)
-    if os.path.exists(f'data/tet_{initial_file}.json'):
-        with open(f'data/tet_{initial_file}.json') as f:
+    if os.path.exists(f"data/tet_{initial_file}.json"):
+        with open(f"data/tet_{initial_file}.json") as f:
             existing_initial_state = json.load(f)
         if recursive_compare(initial_state, existing_initial_state):
             return
         else:
-            print(f'tet_{initial_file}.json out of date, updating')
-    with open(f'data/tet_{initial_file}.json', 'w') as f:
+            print(f"tet_{initial_file}.json out of date, updating")
+    with open(f"data/tet_{initial_file}.json", "w") as f:
         json.dump(initial_state, f)
 
 
 def generate_data(seed, cloud, tet_conc, amp_conc,
-    initial_colony_file, baseline, runtime):
+    initial_colony_file, initial_state_file, baseline,
+    start_time, runtime):
     if baseline:
         run_sim(
             tet_conc=0,
@@ -115,6 +120,7 @@ def generate_data(seed, cloud, tet_conc, amp_conc,
             seed=seed,
             cloud=cloud,
             start_time=0,
+            initial_state_file=initial_state_file,
             baseline=baseline,
             runtime=runtime)
     else:
@@ -124,7 +130,8 @@ def generate_data(seed, cloud, tet_conc, amp_conc,
             seed=seed,
             cloud=cloud,
             initial_colony_file=initial_colony_file,
-            start_time=11550,
+            initial_state_file=initial_state_file,
+            start_time=start_time,
             runtime=runtime
         )
 
@@ -139,8 +146,13 @@ def main():
     parser.add_argument("-b", "--baseline", action="store_true")
     parser.add_argument(
         "-i",
-        "--initial_file",
-        help="colony save state to run the simulation off of",
+        "--initial_colony_file",
+        help="Colony save state to run the simulation off of",
+    )
+    parser.add_argument(
+        "-f",
+        "--initial_state_file",
+        help="Single cell initial state (e.g. from wcEcoli)",
     )
     parser.add_argument(
         "-t",
@@ -163,6 +175,13 @@ def main():
         type=int,
         help="Custom simulation run time."
     )
+    parser.add_argument(
+        "-n",
+        "--start_time",
+        default=None,
+        type=int,
+        help="Custom simulation start time."
+    )
 
     args = parser.parse_args()
 
@@ -171,9 +190,11 @@ def main():
         cloud=(not args.local),
         tet_conc=args.tet_conc,
         amp_conc=args.amp_conc,
-        initial_colony_file=args.initial_file,
+        initial_colony_file=args.initial_colony_file,
+        initial_state_file=args.initial_state_file,
         baseline=args.baseline,
-        runtime=args.runtime
+        runtime=args.runtime,
+        start_time=args.start_time
     )
 
 
