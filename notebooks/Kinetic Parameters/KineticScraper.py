f085469577eed689eb9a560ee7f88069d4f2034d
Aniketh Iyengar <aiyengar03@gmail.com>
2023-02-22 12:34:38 -0800

    Making Script for Retrieving Kinetic Parameters and Updating Google Sheets

--- notebooks/Kinetic Parameters/KineticScraper.py
+++ notebooks/Kinetic Parameters/KineticScraper.py
@@ -64,27 +64,99 @@ def getEnzyme(dicte):
 
 def updateKms(source, substrate, km, params):
     if source not in params:
-        params[source] = {}
+        params[source] = {
+            'substrates': {},
+            'general': {}
+        }
 
-    if substrate in params[source]:
-        params[source][substrate]['km'] = float(km)
+    if substrate in params[source]['substrates']:
+        params[source]['substrates'][substrate]['km'] = float(km)
     else:
-        params[source][substrate] = {
+        params[source]['substrates'][substrate] = {
             'km': float(km),
-            'kcat': -1
+            'kcat': -1,
+            'vmax': -1,
+        }
+
+        params[source]['general'] = {
+            'ph': [],
+            'temperature': []
         }
 
 
 def updateKcats(source, substrate, kcat, params):
     if source not in params:
-        params[source] = {}
+        params[source] = {
+            'substrates': {},
+            'general': {}
+        }
 
-    if substrate in params[source]:
-        params[source][substrate]['kcat'] = float(kcat)
+    if substrate in params[source]['substrates']:
+        params[source]['substrates'][substrate]['kcat'] = float(kcat)
     else:
-        params[source][substrate] = {
+        params[source]['substrates'][substrate] = {
             'km': -1,
-            'kcat': float(kcat)
+            'kcat': float(kcat),
+            'vmax': -1,
+        }
+
+        params[source]['general'] = {
+            'ph': [],
+            'temperature': []
+        }
+
+
+def updateVmaxes(source, substrate, vmax, params):
+    if source not in params:
+        params[source] = {
+            'substrates': {},
+            'general': {}
+        }
+
+    if substrate in params[source]['substrates']:
+        params[source]['substrates'][substrate]['vmax'] = float(vmax)
+    else:
+        params[source]['substrates'][substrate] = {
+            'km': -1,
+            'kcat': -1,
+            'vmax': float(vmax),
+        }
+
+        params[source]['general'] = {
+            'ph': [],
+            'temperature': []
+        }
+
+
+def updatePhs(source, ph, params):
+    if source not in params:
+        params[source] = {
+            'substrates': {},
+            'general': {}
+        }
+
+    if 'ph' in params[source]['general']:
+        params[source]['general']['ph'].append(ph)
+    else:
+        params[source]['general'] = {
+            'ph': [ph],
+            'temperature': []
+        }
+
+
+def updateTemps(source, temp, params):
+    if source not in params:
+        params[source] = {
+            'substrates': {},
+            'general': {}
+        }
+
+    if 'general' in params[source]['general']:
+        params[source]['general']['temperature'].append(float(temp))
+    else:
+        params[source]['general'] = {
+            'ph': [],
+            'temperature': [temp]
         }
 
 
@@ -92,13 +164,19 @@ def findSources(val, sources):
     if 'citation' in val:
         val1 = val['citation']
     else:
-        return tuple("None")
+        return "None", "None"
 
     if type(val1) is list:
         for pub in val1:
-            sources.append(pub['Publication']['@frameid'])
+            if 'pubmed-id' in pub['Publication']:
+                sources.append((pub['Publication']['@frameid'], pub['Publication']['pubmed-id']['#text']))
+            else:
+                sources.append((pub['Publication']['@frameid'], "None"))
     else:
-        sources.append(val1['Publication']['@frameid'])
+        if 'pubmed-id' in val1['Publication']:
+            sources.append((val1['Publication']['@frameid'], val1['Publication']['pubmed-id']['#text']))
+        else:
+            sources.append((val1['Publication']['@frameid'], "None"))
 
     sources = tuple(sorted(sources))
 
@@ -160,7 +238,82 @@ def revaluateSize(dicte, reactions, rxn_index, enzymes, enz_index, params, no_pa
             sources = findSources(dict_kcat, sources)
             updateKcats(sources, substrate, kcat, params)
 
-    if 'km' not in dicte and 'kcat' not in dicte:
+    if 'vmax' in dicte:
+        dict_vmax = dicte['vmax']
+        if type(dict_vmax) is list:
+            for val in dict_vmax:
+                sources = []
+                sources = findSources(val, sources)
+
+                substrate = val['substrate']
+                if 'Compound' in substrate:
+                    substrate = substrate['Compound']['@frameid']
+                else:
+                    substrate = substrate['Protein']['@frameid']
+
+                vmax = val['value']['#text']
+                updateVmaxes(sources, substrate, vmax, params)
+        else:
+            substrate = dict_vmax['substrate']
+            if 'Compound' in substrate:
+                substrate = substrate['Compound']['@frameid']
+            else:
+                substrate = substrate['Protein']['@frameid']
+
+            vmax = dict_vmax['value']['#text']
+            sources = []
+            sources = findSources(dict_vmax, sources)
+            updateVmaxes(sources, substrate, vmax, params)
+
+    if 'ph-opt' in dicte:
+        dict_ph = dicte['ph-opt']
+        if type(dict_ph) is list:
+            for val in dict_ph:
+                sources = []
+                sources = findSources(val, sources)
+
+                if 'value' in val:
+                    ph = val['value']['#text']
+                else:
+                    ph = val['#text']
+
+                updatePhs(sources, ph, params)
+        else:
+            if 'value' in dict_ph:
+                ph = dict_ph['value']['#text']
+            else:
+                ph = dict_ph['#text']
+
+            sources = []
+            sources = findSources(dict_ph, sources)
+            updatePhs(sources, ph, params)
+
+    if 'temperature-opt' in dicte:
+        dict_temp = dicte['temperature-opt']
+        if type(dict_temp) is list:
+            for val in dict_temp:
+                sources = []
+                sources = findSources(val, sources)
+
+                if 'value' in val:
+                    temp = val['value']['#text']
+                else:
+                    temp = val['#text']
+
+                updateTemps(sources, temp, params)
+        else:
+            if 'value' in dict_temp:
+                temp = dict_temp['value']['#text']
+            else:
+                temp = dict_temp['#text']
+
+            sources = []
+            sources = findSources(dict_temp, sources)
+            updateTemps(sources, temp, params)
+
+    if ('km' not in dicte and 'kcat' not in dicte and
+            'vmax' not in dicte and 'ph-opt' not in dicte and
+            'temperature-opt' not in dicte):
         no_params.append([reactions[rxn_index], enzymes[enz_index]])
         del reactions[rxn_index]
         del enzymes[enz_index]
@@ -172,7 +325,8 @@ def revaluateSize(dicte, reactions, rxn_index, enzymes, enz_index, params, no_pa
         enzymes.insert(enz_index, enzymes[enz_index])
 
 
-def updateKinetics(substrates, kcats, kms, dicte, reactions, rxn_index, enzymes, enz_index, no_params):
+def updateKinetics(pubmeds, substrates, kcats, kms, vmaxes, phs, temperatures, dicte, reactions, rxn_index, enzymes,
+                   enz_index, no_params):
     if 'Enzymatic-Reaction' in dicte['ptools-xml']:
         dicte = dicte['ptools-xml']['Enzymatic-Reaction']
 
@@ -181,15 +335,26 @@ def updateKinetics(substrates, kcats, kms, dicte, reactions, rxn_index, enzymes,
     revaluateSize(dicte, reactions, rxn_index, enzymes, enz_index, params, no_params)
 
     for source in params:
-        temp_substrates = list(params[source].keys())
+        temp_substrates = list(params[source]['substrates'].keys())
         temp_km = []
         temp_kcat = []
+        temp_vmax = []
+        temp_ph = []
+        temp_temperature = []
         for substrate in temp_substrates:
-            temp_km.append(params[source][substrate]['km'])
-            temp_kcat.append(params[source][substrate]['kcat'])
+            temp_km.append(params[source]['substrates'][substrate]['km'])
+            temp_kcat.append(params[source]['substrates'][substrate]['kcat'])
+            temp_vmax.append(params[source]['substrates'][substrate]['vmax'])
+        if params[source]['general']:
+            temp_ph = params[source]['general']['ph']
+            temp_temperature = params[source]['general']['temperature']
+        pubmeds.append([x[1] for x in source])
         substrates.append(temp_substrates)
         kcats.append(temp_kcat)
         kms.append(temp_km)
+        vmaxes.append(temp_vmax)
+        phs.append(temp_ph)
+        temperatures.append(temp_temperature)
 
 
 # Establish Session
@@ -207,7 +372,7 @@ not_found_genes = []
 for gene in genes:
     getEnzyme_Rxns(gene, enzyme_rxns, not_found_genes)
 
-reactions, enzymes, kms, kcats, substrates, temperatures = [], [], [], [], [], []
+pubmeds, reactions, enzymes, kms, kcats, substrates, phs, temperatures, vmaxes = [], [], [], [], [], [], [], [], []
 
 # Extract Information
 no_params = []
@@ -217,19 +382,21 @@ for rxn in enzyme_rxns:
     dicte = etree_to_dict(tree)
     reactions.append(getReaction(dicte))
     enzymes.append([getEnzyme(dicte)])
-    updateKinetics(substrates, kcats, kms, dicte, reactions, len(reactions) - 1, enzymes, len(enzymes) - 1, no_params)
-
-    if len(reactions) + len(enzymes) + len(kms) + len(kcats) != 4 * len(reactions):
-        print(1)
+    updateKinetics(pubmeds, substrates, kcats, kms, vmaxes, phs, temperatures, dicte, reactions, len(reactions) - 1, enzymes,
+                   len(enzymes) - 1, no_params)
 
 information = {
+    'pubmedID': pubmeds,
     'reactionID': reactions,
     'enzymeID': enzymes,
     'substrateIDs': substrates,
     'kcat (1/s)': kcats,
-    'kM (uM)': kms
+    'kM (uM)': kms,
+    'vmax': vmaxes,
+    'ph': phs,
+    'temperature': temperatures
 }
 
-df = pd.DataFrame.from_dict(information)
+output = pd.DataFrame.from_dict(information)
+output.to_csv("params.csv")
 
-print(1)
