b4d44bc4dfe24d1c5fcaff5c38e2453ac6b09a3c
thalassemia <67928790+thalassemia@users.noreply.github.com>
2023-09-19 17:57:54 -0700

    Get auto-documentation working again

--- ecoli/analysis/antibiotics_colony/__init__.py
+++ ecoli/analysis/antibiotics_colony/__init__.py
@@ -1,11 +1,14 @@
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.constants import N_A
+from wholecell.utils.filepath import ROOT_PATH
 
 plt.rcParams['svg.fonttype'] = 'none'
 plt.rcParams['font.family'] = 'Arial'
 
-DE_GENES = pd.read_csv('data/marA_binding/model_degenes.csv')
+DE_GENES = pd.read_csv(os.path.join(ROOT_PATH,
+    'data/marA_binding/model_degenes.csv'))
 SPLIT_TIME = 11550
 MAX_TIME = 26000
 COUNTS_PER_FL_TO_NANOMOLAR = 1 / (1e-15) / N_A * (1e9)
