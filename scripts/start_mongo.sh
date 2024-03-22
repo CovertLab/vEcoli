abd11e2c5487988b14b2f8cf81bea29c0cf07932
thalassemia <67928790+thalassemia@users.noreply.github.com>
2024-03-21 18:29:41 -0700

    More work on Google Cloud support

--- scripts/start_mongo.sh
+++ scripts/start_mongo.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
-# Read MongoDB password
-echo -n Create a password for MongoDB:
-read -s password
-echo -e \n
-# Set MongoDB authentication environment variables
-CONTAINER_ENV="MONGO_INITDB_ROOT_USERNAME=vecoli"
-CONTAINER_ENV="$CONTAINER_ENV,MONGO_INITDB_ROOT_PASSWORD=$password"
-
-gcloud compute instances create-with-container mongodb-vecoli \
-    --zone=us-central1-a \
-    --container-env=$CONTAINER_ENV \
-    --machine-type=e2-standard-16 \
+set -eu
+MONGO_DISK_SIZE_GB="${1:-2500}"
+# Get VM name from account name (left of "@" in email)
+gcp_account=$(gcloud config get-value account)
+IFS='@' read -ra gcp_account <<< $gcp_account
+gcp_account=${gcp_account[0]}
+vm_name=${gcp_account}-mongodb
+snapshot_data=$(gcloud compute snapshots list \
+    --filter="name:'final-antibiotic*'" \
+    --format="value(name,diskSizeGb)" \
+    --sort-by="~creationTimestamp" 2>&1)
+# If no snapshot exists, create fresh disk
+if [ -z "${snapshot_data}" ]; then
+    disk_arg="--create-disk=name=${vm_name}-data,size=${MONGO_DISK_SIZE_GB}GB,type=pd-balanced"
+    echo "Creating new disk of size ${MONGO_DISK_SIZE_GB} GB."
+# Otherwise, restore from most recently created snapshot
+else
+    echo "Restoring from snapshot ${snapshot_data[0]}" \
+        "to disk of size ${snapshot_data[1]} GB."
+    IFS=$'\t' read -ra snapshot_data <<< $snapshot_data
+    gcloud compute disks create ${vm_name}-data \
+        --size=${snapshot_data[1]} \
+        --source-snapshot=${snapshot_data[0]}
+    disk_arg="--disk=name=${vm_name}-data"
+    echo "Done restoring from snapshot."
+fi
+# Create VM with latest MongoDB image
+gcloud compute instances create-with-container ${vm_name} \
+    --machine-type=e2-standard-2 \
     --boot-disk-size=10GB \
     --boot-disk-type=pd-balanced \
-    --create-disk=name=mongodb-vecoli-data,size=2500GB,type=pd-balanced \
-    --container-arg=--dbpath=/scratch \
+    ${disk_arg} \
+    --container-command="/bin/bash" \
+    --container-arg="-c" \
+    --container-arg="chmod 777 /data/db; \
+        docker-entrypoint.sh mongod" \
     --container-image=mongo:latest \
-    --container-mount-disk=name=mongodb-vecoli-data,mount-path=/scratch
-gcloud compute instances start mongodb-vecoli
+    --container-mount-disk=mount-path=/data/db \
+    --no-address
+echo "Done creating ${vm_name}."
