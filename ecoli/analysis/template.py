4dcd4823982ea2677d17b45c8a8008eaaab75b90
thalassemia <67928790+thalassemia@users.noreply.github.com>
2024-04-19 17:20:00 -0700

    Work on Polars interface

--- ecoli/analysis/template.py
+++ ecoli/analysis/template.py
@@ -1,9 +1,15 @@
+"""
+Cookbook of common Polars manipulations for analysis scripts.
+"""
+
 import pickle
+from itertools import pairwise
+from typing import Any
 
-from pyarrow import compute as pc
-from pyarrow import dataset as ds
+import numpy as np
+import polars as pl
 
-from ecoli.library.parquet_emitter import get_datasets
+from ecoli.library.parquet_emitter import get_lazyframes
 
 METADATA_PREFIX = 'data__output_metadata__'
 """
@@ -11,6 +17,76 @@ In the config dataset, user-defined metadata for each store
 (see :py:meth:`~ecoli.experiments.ecoli_master_sim.EcoliSim.get_output_metadata`)
 will be contained in columns with this prefix.
 """
+
+
+def ndlist_to_ndarray(s: pl.Series) -> np.ndarray:
+    """
+    Convert a series consisting of nested lists with fixed dimensions into
+    a Numpy ndarray. This should really only be necessary if you are trying
+    to perform linear algebra (e.g. matrix multiplication, dot products).
+    You can likely accomplish most other manipulations with Polars alone.
+    """
+    dimensions = [1, len(s)]
+    while s.dtype == pl.List:
+        s = s.explode()
+        dimensions.append(len(s))
+    dimensions = [p[1] // p[0] for p in pairwise(dimensions)]
+    return s.to_numpy().reshape(dimensions)
+
+
+def ndidx_to_pl_expr(name: str, idx: list[Any]) -> pl.Expr:
+    """
+    Returns a Polars expression that evaluates to the equivalent of converting
+    the ``name`` column into an ndarray (see :py:func:`~.ndlist_to_ndarray`)
+    and calling ``name_arr[idx]`` (only integer indices or ``":"``). Should
+    really only be used for columns with lists of 2+ dimensions. To select
+    elements from an unnested (1D) list column, prefer :py:func:`~.named_idx`. 
+
+    Args:
+        name: Name of column to recursively index
+        idx: Sequence of indices in order, one for each dimension to index. To
+            get all elements for a dimension, supply the string ``":"``.
+            Here are some examples::
+
+                [0, 1] # First row, second column
+                [[0, 1], 1] # First and second row, second column
+                [0, 1, ":"] # First element of axis 1, second of 2, all of 3
+
+    """
+    idx = idx.copy().reverse()
+    # Construct expression from inside out (deepest to shallowest axis)
+    pl_expr = pl.element().list.gather(idx.pop(0))
+    for indices in idx[:-1]:
+        # Skip gathering indices for dimensions where all are selected
+        if indices == ':':
+            pl_expr = pl.element().list.eval(pl_expr)
+        else:
+            pl_expr = pl.element().list.gather(indices).list.eval(pl_expr)
+    if idx[-1] == ':':
+        return pl.col(name).list.eval(pl_expr)
+    return pl.col(name).list.gather(idx[-1]).list.eval(pl_expr)
+
+
+def named_idx(col: str, names: list[str], idx: list[int]) -> dict[str, pl.Expr]:
+    """
+    Returns a mapping of column names to Polars expressions that get
+    given indices from each row of a list column.
+
+    Args:
+        col: Name of list column.
+        names: Suffixes to append to ``col`` for final column names
+        idx: For each suffix in ``names``, the index to get from each
+            row of the ``col``
+    
+    Returns:
+        Dictionary that maps ``{col}__{names[i]}`` to Polars expression for
+        ``idx[i]``th element of each row in ``col``
+    """
+    return {
+        f'{col}__{name}': pl.col('col').list.get(index)
+        for name, index in zip(names, idx)
+    }
+
 
 def analysis_template(config, sim_data_path, validation_data_path):
     """
@@ -22,36 +98,27 @@ def analysis_template(config, sim_data_path, validation_data_path):
     with open(validation_data_path, 'rb') as f:
         validation_data = pickle.load(f)
 
-    # Load Parquet datasets from output directory / URI specified in config
+    # Load Parquet files from output directory / URI specified in config
     emitter_config = config['emitter']['config']
-    config_ds, history_ds = get_datasets(emitter_config.get('out_dir', None),
-                                         emitter_config.get('out_uri', None))
+    config_lf, history_lf = get_lazyframes(
+        emitter_config.get('out_dir', None),
+        emitter_config.get('out_uri', None))
 
     # Create filters for the data you want to read
-    exp_id_filter = ds.field('experiment_id') == 'some_experiment_id'
-    variant_filter = ds.field('variant') == 'some_variant_name'
-    seed_filter = ds.field('seed') == 0
-    generation_filer = ds.field('generation') == 2
-    agent_filter = ds.field('agent_id') == '01'
-    many_seed_filter = ds.field('seed').isin([1,2,3])
-    seed_range_filter = ds.field('seed') <= 100
+    exp_id_filter = pl.col('experiment_id') == 'some_experiment_id'
+    generation_range_filter = pl.col('generation').is_in(list(range(4,8)))
+    mass_filter = pl.col('listeners__mass__dry_mass') < 300
     # Filters support logical &, ~, and |
-    layered_filter = exp_id_filter & variant_filter & seed_filter
-    exclude_seeds = ~many_seed_filter
-    combined_seeds = seed_filter | many_seed_filter
-
-    # Make sure your preliminary filter condition above only includes the
-    # experiment_id, variant, seed, generation, or agent_id columns. This
-    # allows us to retrieve the configuration data for the specified set
-    # of simulations.
-    configs = config_ds.to_table(filter=layered_filter)
+    # Construct your query step-by-step and call ``collect`` when ready.
+    config_lf = config_lf.filter(exp_id_filter & generation_range_filter)
+    history_lf = history_lf.filter(exp_id_filter | mass_filter)
 
     # Read values of interest from configs, such as field-specific metadata
     # Say we wanted to get the indices of certain bulk molecules in the
     # bulk counts array by name for agent_id 01
     molecules_of_interest = ['GUANOSINE-5DP-3DP[c]', 'WATER[c]', 'PROTON[c]']
-    bulk_names = configs.filter(ds.field('agent_id') == '01').column(
-        METADATA_PREFIX + 'bulk')[0].values
+    bulk_names = config_lf.select(METADATA_PREFIX + 'bulk'
+        ).collect()[METADATA_PREFIX + 'bulk'][0]
     indices_of_interest = {}
     for mol in molecules_of_interest:
         indices_of_interest[mol] = bulk_names.index(mol)
