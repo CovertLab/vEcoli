b18eb0bd6b76be7c46bef7b81ec15803e0f6d6ec
thalassemia <67928790+thalassemia@users.noreply.github.com>
2024-06-27 11:58:09 -0700

    String replace argument must be string

--- scripts/workflow.py
+++ scripts/workflow.py
@@ -352,7 +352,8 @@ def main():
     nf_template = "".join(nf_template)
     nf_template = nf_template.replace("IMPORTS", sim_imports)
     nf_template = nf_template.replace("WORKFLOW", sim_workflow)
-    nf_template = nf_template.replace("PARCA_CPUS", config["parca_options"]["cpus"])
+    nf_template = nf_template.replace("PARCA_CPUS", str(
+        config["parca_options"]["cpus"]))
     local_workflow = os.path.join(local_outdir, "main.nf")
     with open(local_workflow, "w") as f:
         f.writelines(nf_template)
