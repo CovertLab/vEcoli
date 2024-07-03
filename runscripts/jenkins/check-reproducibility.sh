89b79a0cac32978f94decca2c7ee038c9d1e3466
thalassemia <cheah_sean@yahoo.com>
2024-07-02 21:43:56 -0700

    Jenkins reproducibility and two gen tests

--- runscripts/jenkins/check-reproducibility.sh
+++ runscripts/jenkins/check-reproducibility.sh
@@ -9,6 +9,7 @@ set -eu
 
 script_dir=$(dirname $(dirname $(realpath $0)))
 out_dir=$(dirname $script_dir)/out
+data_dir=$(dirname $script_dir)/data
 dir1="check-reproducibility-1"
 dir2="check-reproducibility-2"
 git_hash=$(git rev-parse HEAD)
@@ -16,25 +17,30 @@ git_hash=$(git rev-parse HEAD)
 source $script_dir/jenkins/setup-environment.sh
 
 # Run parca twice and check that output is consistent from run to run
-python $script_dir/parca.py -c 4 -o $out_dir/$dir1-$git_hash --save-intermediates
-python $script_dir/parca.py -c 4 -o $out_dir/$dir2-$git_hash --save-intermediates
-$script_dir/debug/compare_pickles.py $out_dir/$dir1-$git_hash/kb $out_dir/$dir2-$git_hash/kb
+python $script_dir/parca.py -c 4 -o "$out_dir/$dir1-$git_hash"
+python $script_dir/parca.py -c 4 -o "$out_dir/$dir2-$git_hash"
+python $script_dir/debug/compare_pickles.py "$out_dir/$dir1-$git_hash/kb" "$out_dir/$dir2-$git_hash/kb"
 
 # Run entire simulation for each parca output
 python $(dirname $script_dir)/ecoli/experiments/ecoli_master_sim.py \
     --config $script_dir/jenkins/configs/reproducibility.json \
-    --experiment_id $dir1-$git_hash --daughter_outdir $out_dir/$dir1-$git_hash \
-    --sim_data_path $out_dir/$dir1-$git_hash/kb/simData.cPickle
+    --experiment_id "$dir1-$git_hash" --daughter_outdir "$out_dir/$dir1-$git_hash" \
+    --sim_data_path "$out_dir/$dir1-$git_hash/kb/simData.cPickle" --fail_at_total_time
 python $(dirname $script_dir)/ecoli/experiments/ecoli_master_sim.py \
     --config $script_dir/jenkins/configs/reproducibility.json \
-    --experiment_id $dir2-$git_hash --daughter_outdir $out_dir/$dir2-$git_hash \
-    --sim_data_path $out_dir/$dir2-$git_hash/kb/simData.cPickle
+    --experiment_id "$dir2-$git_hash" --daughter_outdir "$out_dir/$dir2-$git_hash" \
+    --sim_data_path "$out_dir/$dir2-$git_hash/kb/simData.cPickle" --fail_at_total_time
 
 # Run short daughters to check that everything including division is consistent
+ln -s "$out_dir/$dir1-$git_hash/daughter_state_0.json" $data_dir/daughter_state_0.json
 python $(dirname $script_dir)/ecoli/experiments/ecoli_master_sim.py \
     --config $script_dir/jenkins/configs/reproducibility.json \
-    --experiment_id $dir1-$git_hash --agent_id "00" --total_time 10
+    --experiment_id "$dir1-$git_hash" --agent_id "00" --total_time 10 \
+    --initial_state_file daughter_state_0
+rm $data_dir/daughter_state_0.json
+ln -s "$out_dir/$dir2-$git_hash/daughter_state_0.json" $data_dir/daughter_state_0.json
 python $(dirname $script_dir)/ecoli/experiments/ecoli_master_sim.py \
     --config $script_dir/jenkins/configs/reproducibility.json \
-    --experiment_id $dir2-$git_hash --agent_id "01" --total_time 10
-$script_dir/debug/diff_simouts.py $out_dir/$dir1-$git_hash $out_dir/$dir2-$git_hash
+    --experiment_id "$dir2-$git_hash" --agent_id "00" --total_time 10 \
+    --initial_state_file daughter_state_0
+python $script_dir/debug/diff_simouts.py -o "$out_dir" "$dir1-$git_hash" "$dir2-$git_hash"
