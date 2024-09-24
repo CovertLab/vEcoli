a20621d367a2150e4dde784a3a48753679d97b10
Eran <agmon.eran@gmail.com>
2024-09-23 21:49:49 -0400

    investigating kinetic parameters

--- ecoli/experiments/ecoli_basic.py
+++ ecoli/experiments/ecoli_basic.py
@@ -1,16 +1,28 @@
-import numpy as np
+"""
+
+Ecoli Composer.
+- ecoli/composites/ecoli_master.py
+
+EcoliSim. The main interface for running the ecoli model
+- ecoli/experiments/ecoli_master_sim.py
+
+Metabolism process
+- ecoli/processes/metabolism.py
+
+Metabolism dataclass:
+- reconstruction/ecoli/dataclasses/process/metabolism.py
+"""
+
 from vivarium.core.engine import pf
-from ecoli.experiments.ecoli_master_sim import EcoliSim, CONFIG_DIR_PATH
+from ecoli.experiments.ecoli_master_sim import EcoliSim
 
 
 def run_default():
     sim = EcoliSim.from_file()
     sim.total_time = 2
-
-
     print(f'INITIAL PROCESS CONFIGS: {pf(sim.config["process_configs"])}')
 
-    # update process configs
+    # # update process configs
     sim.override_config({
         'process_configs': {
             'ecoli-metabolism': {
@@ -18,21 +30,30 @@ def run_default():
             }
         }
     })
-    print(f'NEW PROCESS CONFIGS: {pf(sim.config["process_configs"])}')
+    # print(f'NEW PROCESS CONFIGS: {pf(sim.config["process_configs"])}')
 
     # build the ecoli model
     sim.build_ecoli()
 
-    # # inspect the sim object
-    # process_configs = sim.config['process_configs']
-    # sim.processes['ecoli-metabolism']
+    # retrieve kinetic parameters
+    sim.processes['ecoli-metabolism'].parameters['metabolism'].kinetic_constraint_reactions
+    sim.processes['ecoli-metabolism'].parameters['metabolism'].kinetic_constraint_enzymes
+    sim.processes['ecoli-metabolism'].parameters['metabolism'].kinetic_constraint_substrates
+    sim.processes['ecoli-metabolism'].parameters['metabolism']._kcats
+    sim.processes['ecoli-metabolism'].parameters['metabolism']._saturations
+    sim.processes['ecoli-metabolism'].parameters['metabolism']._enzymes
+    sim.processes['ecoli-metabolism'].parameters['metabolism'].constraint_is_kcat_only
+    sim.processes['ecoli-metabolism'].parameters['metabolism']._kinetic_constraints
 
+    # # get the kinetic constraints function
+    # # this is the way to get kinetic constraints given the enzymes and substrates
+    # sim.processes['ecoli-metabolism'].parameters['metabolism'].get_kinetic_constraints(enzymes: Unum, substrates: Unum)
 
     # run the simulation
     sim.run()
 
     # get the data
-    # sim.emitter.
+    data = sim.emitter.get_data()
 
 
 if __name__ == "__main__":
