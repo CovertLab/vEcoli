3c7a71846d3fc4771c50e8edfd74b95c0671cd34
Cyrus <cyrus.bio@pm.me>
2024-10-29 10:20:07 -0700

    Added files to use.

--- ecoli/experiments/metabolism_redux_sim.py
+++ ecoli/experiments/metabolism_redux_sim.py
@@ -53,7 +53,7 @@ def run_ecoli_with_metabolism_redux(
 # disables growth rate control
 def run_ecoli_with_metabolism_redux_classic(
     filename="metabolism_redux_classic",
-    total_time=3000,
+    total_time=1300,
     divide=True,
     # initial_state_file='wcecoli_t0', # 'met_division_test_state',
     progress_bar=True,
