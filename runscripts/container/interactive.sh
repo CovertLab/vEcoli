e1c9d6bdc3ad0e0589a4ade49a6b2f9d6edb77e9
thalassemia <cheah_sean@yahoo.com>
2024-11-30 03:21:36 -0800

    Script to launch container for interactive debugging

---
+++ runscripts/container/interactive.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Start an interactive Docker or Apptainer container from image built using
+# build-wcm.sh
+
+set -eu
+
+WCM_IMAGE="${USER}-wcm-code"
+BUILD_APPTAINER=0
+
+usage_str="Usage: interactive.sh [-w WCM_IMAGE] [-b BIND_PATH] [-a]\n\
+Options:\n\
+    -w: Path of Apptainer wcm-code image to load if -a, otherwise Docker \
+tag; defaults to "$USER-wcm-code".\n\
+    -a: Load Apptainer image.\n"
+
+print_usage() {
+  printf "$usage_str"
+}
+
+while getopts 'w:a' flag; do
+  case "${flag}" in
+    w) WCM_IMAGE="${OPTARG}" ;;
+    a) BUILD_APPTAINER=1 ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if (( $BUILD_APPTAINER )); then
+    echo "=== Launching Apptainer container from ${WCM_IMAGE} ==="
+    apptainer shell ${WCM_IMAGE}
+else
+    echo "=== Launching Docker container from ${WCM_IMAGE} ==="
+    docker container run -it ${WCM_IMAGE} bash
+fi
