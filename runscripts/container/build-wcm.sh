6b71cfa934f1959648510c74b23dad95e42a34e2
thalassemia <cheah_sean@yahoo.com>
2024-11-30 04:44:20 -0800

    No need for WCM container image on Sherlock

--- runscripts/container/build-wcm.sh
+++ runscripts/container/build-wcm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Use Google Cloud Build, local Docker, or HPC cluster Apptainer to build a
-# personalized image with current state of the vEcoli repo. If using Cloud
-# Build, store the built image in the "vecoli" repository in Artifact Registry.
+# Use Google Cloud Build or local Docker to build a personalized image with
+# current state of the vEcoli repo. If using Cloud Build, store the built
+# image in the "vecoli" repository in Artifact Registry.
 #
 # ASSUMES: The current working dir is the vEcoli/ project root.
 
@@ -10,20 +10,12 @@ set -eu
 RUNTIME_IMAGE="${USER}-wcm-runtime"
 WCM_IMAGE="${USER}-wcm-code"
 RUN_LOCAL=0
-BUILD_APPTAINER=0
-BIND_PATHS=''
-OUTDIR=''
 
 usage_str="Usage: build-wcm.sh [-r RUNTIME_IMAGE] \
 [-w WCM_IMAGE] [-a] [-b BIND_PATH] [-l]\n\
-    -r: Path of Apptainer wcm-runtime image to build from if -a, otherwise \
-Docker tag; defaults to "$USER-wcm-runtime" (must exist in Artifact Registry \
-if Docker tag).\n\
-    -w: Path of Apptainer wcm-code image to build if -a, otherwise Docker \
-tag; defaults to "$USER-wcm-code".\n\
-    -a: Build Apptainer image (cannot use with -l).\n\
-    -b: Absolute path to bind to Apptainer image for workflow output \
-(only works with -a).\n\
+    -r: Docker tag of wcm-runtime image to build from; defaults to \
+"$USER-wcm-runtime" (must exist in Artifact Registry).\n\
+    -w: Docker tag of wcm-code image to build; defaults to "$USER-wcm-code".\n\
     -l: Build image locally.\n"
 
 print_usage() {
@@ -34,9 +26,7 @@ while getopts 'r:w:abl:' flag; do
   case "${flag}" in
     r) RUNTIME_IMAGE="${OPTARG}" ;;
     w) WCM_IMAGE="${OPTARG}" ;;
-    l) (( $BUILD_APPTAINER )) && print_usage && exit 1 || RUN_LOCAL=1 ;;
-    a) (( $RUN_LOCAL )) && print_usage && exit 1 || BUILD_APPTAINER=1 ;;
-    b) (( $RUN_LOCAL )) && print_usage && exit 1 || BIND_PATHS="-B ${OPTARG}" && OUTDIR="${OPTARG}" ;;
+    l) RUN_LOCAL=1 ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -56,15 +46,6 @@ if (( $RUN_LOCAL )); then
         --build-arg git_hash="${GIT_HASH}" \
         --build-arg git_branch="${GIT_BRANCH}" \
         --build-arg timestamp="${TIMESTAMP}" .
-elif (( $BUILD_APPTAINER )); then
-    echo "=== Building WCM code Apptainer Image ${WCM_IMAGE} on ${RUNTIME_IMAGE} ==="
-    apptainer build ${BIND_PATHS} \
-        --build-arg runtime_image="${RUNTIME_IMAGE}" \
-        --build-arg git_hash="${GIT_HASH}" \
-        --build-arg git_branch="${GIT_BRANCH}" \
-        --build-arg timestamp="${TIMESTAMP}" \
-        --build-arg outdir="${OUTDIR}" \
-        ${WCM_IMAGE} runscripts/container/wholecell/Singularity
 else
     echo "=== Cloud-building WCM code Docker Image ${WCM_IMAGE} on ${RUNTIME_IMAGE} ==="
     echo "=== git hash ${GIT_HASH}, git branch ${GIT_BRANCH} ==="
