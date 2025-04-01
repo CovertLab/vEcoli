91746f59aad7c4bb8e851629b80e2a583c033130
thalassemia <cheah_sean@yahoo.com>
2025-03-31 21:40:22 -0700

    Fix two documentation links

--- ecoli/library/schema.py
+++ ecoli/library/schema.py
@@ -569,7 +569,7 @@ def listener_schema(elements: Dict[str, Any]) -> Dict[str, Dict[str, Any]]:
             is metadata that will be emitted at the beginning of a simulation
             (see :py:meth:`~ecoli.experiments.ecoli_master_sim.EcoliSim.get_output_metadata`).
             This metadata can then be retrieved later to aid in interpreting
-            listener values (see :py:func:`~ecoli.library.schema.get_field_metadata`).
+            listener values (see :py:func:`~ecoli.library.parquet_emitter.get_field_metadata`).
             As an example, this metadata might be an array of molecule names
             for a listener whose emits are arrays of counts, where the nth
             molecule name in the metadata corresponds to the nth value in the
