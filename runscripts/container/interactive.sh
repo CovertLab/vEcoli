45c4c339948bafe2c21dd30a3cc43d3cee2d80da
thalassemia <cheah_sean@yahoo.com>
2025-04-01 17:30:03 -0700

    fix

--- runscripts/container/interactive.sh
+++ runscripts/container/interactive.sh
@@ -116,13 +116,14 @@ if (($USE_APPTAINER)); then
   mkfs.ext3 -F ${TMP_OVERLAY_DIR}/overlay.img
   if (($DEV_MODE)); then
     # Create editable install of current directory
-    EXEC_CMD="bash -c \"UV_PROJECT_ENVIRONMENT=/vEcoli/.venv uv sync --frozen && exec bash\""
+    EXEC_CMD="bash -c 'UV_PROJECT_ENVIRONMENT=/vEcoli/.venv \
+      uv sync --frozen && exec bash'"
   else
     EXEC_CMD="bash"
   fi
   # Fakeroot necessary for overlay to work
   apptainer exec -e --overlay ${TMP_OVERLAY_DIR}/overlay.img \
-    --fakeroot ${BIND_STR} ${IMAGE_NAME} ${EXEC_CMD}
+    --fakeroot ${BIND_STR} ${IMAGE_NAME} eval ${EXEC_CMD}
 else
   # Docker-specific logic
   if ((!$RUN_LOCAL)); then
@@ -151,10 +152,11 @@ else
 
   if (($DEV_MODE)); then
     # Create editable install of current directory
-    EXEC_CMD="bash -c \"UV_PROJECT_ENVIRONMENT=/vEcoli/.venv uv sync --frozen && exec bash\""
+    EXEC_CMD="bash -c 'UV_PROJECT_ENVIRONMENT=/vEcoli/.venv \
+      uv sync --frozen && exec bash'"
   else
     EXEC_CMD="bash"
   fi
 
-  docker container run -it ${BIND_STR} ${IMAGE_NAME} ${EXEC_CMD}
+  docker container run -it ${BIND_STR} ${IMAGE_NAME} eval ${EXEC_CMD}
 fi
