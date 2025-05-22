5cdbdcf3068cd0ea77230b65baa0b02222863eda
thalassemia <cheah_sean@yahoo.com>
2025-05-22 02:16:10 -0700

    Remove obsolete migration tests and data

--- migration/__init__.py
+++
@@ -1,4 +0,0 @@
-from ecoli.library.sim_data import LoadSimData, SIM_DATA_PATH, SIM_DATA_PATH_NO_OPERONS
-
-LOAD_SIM_DATA = LoadSimData(sim_data_path=SIM_DATA_PATH, seed=0)
-LOAD_SIM_DATA_NO_OPERONS = LoadSimData(sim_data_path=SIM_DATA_PATH_NO_OPERONS, seed=0)
